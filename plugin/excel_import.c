c98d600d23f2fccc99ef0737b5d4b02b91c0699d
Allin Cottrell <cottrell@wfu.edu>
2002-03-24 17:42:51 +0000

    excel: add support for MULRK, fix memory leak.

--- plugin/excel_import.c
+++ plugin/excel_import.c
@@ -10,20 +10,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <glib.h>
 #include "xltypes.h"
 
 static void print_sheet (void);
 static void free_sheet (void);
 static void print_value (char *value);
 static int getshort (char *rec,int offset);
-static void process_item (int rectype, int reclen, char *rec); 
-static void allocate (int row, int col);
+static int process_item (int rectype, int reclen, char *rec); 
+static int allocate (int row, int col);
 static char *copy_unicode_string (char **src);
-static char *convert8to8 (char *src, int count);
-static char *convert16to8 (char *src, int count);
+static char *convert8to7 (char *src, int count);
+static char *convert16to7 (char *src, int count);
 static char *mark_string (char *instr);
 
 char cell_separator = ',';
+
+/* #define EDEBUG 1 */
 
 static char *format_double (char *rec, int offset) 
 {	
@@ -35,7 +38,7 @@ static char *format_double (char *rec, int offset)
     char *d, *s;
     int i;
 
-#ifdef WORDS_BIGENDIAN     
+#if G_BYTE_ORDER == G_BIG_ENDIAN
     for (s=rec+offset+8, d=dconv.cc, i=0; i<8; i++) *(d++) = *(--s);
 #else       
     for (s=rec+offset, d=dconv.cc, i=0; i<8; i++) *(d++) = *(s++);
@@ -44,13 +47,13 @@ static char *format_double (char *rec, int offset)
     return buffer;
 }
 
-static void do_table (FILE *input, char *filename) 
+static int do_table (FILE *input, char *filename) 
 {    
     long rectype;
     long reclen;
     int eof_flag = 0;
     unsigned char rec[MAX_MS_RECSIZE];
-    int itemsread = 1;
+    int err = 0, itemsread = 1;
 
     while (itemsread) {
 	fread(rec, 2, 1, input);
@@ -64,17 +67,17 @@ static void do_table (FILE *input, char *filename)
 		break;
 	    } else {
 		fprintf(stderr, "%s: Invalid BOF record\n", filename);
-	        return;
+	        return 1;
 	    } 
 	}
     }    
 
     if (feof(input)) {
 	fprintf(stderr, "%s: No BOF record found\n", filename);
-	exit(1);
+	return 1;
     }  
    
-    while (itemsread) {
+    while (!err && itemsread) {
 	char buffer[2];
 
 	rectype = 0;
@@ -90,21 +93,20 @@ static void do_table (FILE *input, char *filename)
 	    itemsread = fread(rec, 1, reclen, input);
 	    rec[reclen] = '\0';
 	}
-	if (eof_flag) {
-	    if (rectype != BOF) {
-		break;
-	    }    
+	if (eof_flag && rectype != BOF) 
+	    break;
+	if (process_item(rectype, reclen, rec)) { 
+	    err = 1;
+	    break;
 	}
-	process_item(rectype, reclen, rec);
-	if (rectype == MSEOF) {
+	if (rectype == MSEOF) 
 	    eof_flag = 1;
-	} else {
+	else 
 	    eof_flag = 0;	
-	}  
     }
 
     fclose(input);
-    return;
+    return err;
 }
 
 struct rowdescr {
@@ -119,21 +121,91 @@ struct rowdescr *rowptr = NULL;
 char **saved_reference = NULL;
 int startrow = 0, lastrow = 0;
 
-static void process_item (int rectype, int reclen, char *rec) 
+static double get_le_double (const void *p)
+{
+#if G_BYTE_ORDER == G_BIG_ENDIAN
+        if (sizeof (double) == 8) {
+                double  d;
+                int     i;
+                guint8 *t  = (guint8 *)&d;
+                guint8 *p2 = (guint8 *)p;
+                int     sd = sizeof (d);
+
+                for (i = 0; i < sd; i++)
+                        t[i] = p2[sd - 1 - i];
+
+                return d;
+        } else {
+                g_error ("Big endian machine, but weird size of doubles");
+        }
+#elif G_BYTE_ORDER == G_LITTLE_ENDIAN
+        if (sizeof (double) == 8) {
+                double data;
+
+                memcpy (&data, p, sizeof (data));
+                return data;
+        } else {
+                g_error ("Little endian machine, but weird size of doubles");
+        }
+#else
+#error "Byte order not recognised -- out of luck"
+#endif
+}
+
+#define MS_OLE_GET_GUINT32(p) (guint32)(*((const guint8 *)(p)+0) |        \
+                                        (*((const guint8 *)(p)+1)<<8) |   \
+                                        (*((const guint8 *)(p)+2)<<16) |  \
+                                        (*((const guint8 *)(p)+3)<<24))
+
+static double biff_get_rk (const unsigned char *ptr)
+{
+    gint32 number;
+    enum eType {
+	eIEEE = 0, eIEEEx100 = 1, eInt = 2, eIntx100 = 3
+    } type;
+
+    number = MS_OLE_GET_GUINT32 (ptr);
+    type = (number & 0x3);
+    switch (type) {
+    case eIEEE:
+    case eIEEEx100:
+        {
+	    guint8 tmp[8];
+	    double answer;
+	    int lp;
+
+	    for (lp = 0; lp < 4; lp++) {
+		tmp[lp + 4]= (lp > 0) ? ptr[lp]: (ptr[lp] & 0xfc);
+		tmp[lp]=0;
+	    }
+	    answer = get_le_double (tmp);
+	    return (type == eIEEEx100)? answer / 100 : answer;
+        }
+    case eInt:
+	return (double) (number >> 2);
+    case eIntx100:
+	number >>= 2;
+	if ((number % 100) == 0)
+	    return (double) (number/100);
+	else
+	    return (double) (number/100.0);
+    }
+    return -999.0;
+}
+
+static int process_item (int rectype, int reclen, char *rec) 
 {
     switch (rectype) {
-    case CODEPAGE: 
-	break;
-    case FORMAT: 
-	break;
     case SST: {
 	char *ptr = rec + 8, **outptr;
 	int i;
 
 	sstsize = getshort(rec, 4);
 	sst = malloc(sstsize * sizeof(char *));
-	fprintf(stderr, "malloced sst at %d bytes, %p\n", 
+#ifdef EDEBUG
+	fprintf(stderr, "Got SST: malloced sst at %d bytes, %p\n", 
 		sstsize * sizeof(char *), (void *) sst);
+#endif
 	for (i=0, outptr=sst; i<sstsize && (ptr-rec)<reclen; i++, outptr++) 
 	    *outptr = copy_unicode_string(&ptr);
 	if (i < sstsize) 
@@ -143,6 +215,7 @@ static void process_item (int rectype, int reclen, char *rec)
     case CONTINUE: {
 	int i = sstnext;
 	char **outptr, *ptr = rec;
+
 	if (i == 0) 
 	    break;
 	for (outptr=sst+i; i<sstsize && (ptr-rec)<reclen; i++, outptr++) 
@@ -159,9 +232,12 @@ static void process_item (int rectype, int reclen, char *rec)
 	row = getshort(rec, 0); 
 	col = getshort(rec, 2);
 	len = getshort(rec, 6);
-	allocate(row, col);
+#ifdef EDEBUG
+	fprintf(stderr, "Got LABEL, row=%d, col=%d\n", row, col);
+#endif
+	if (allocate(row, col)) return 1;
 	prow = rowptr + row;
-	prow->cells[col] = mark_string(convert8to8(rec+8, len));
+	prow->cells[col] = mark_string(convert8to7(rec+8, len));
 	break;
     }     
     case CONSTANT_STRING: {
@@ -170,8 +246,11 @@ static void process_item (int rectype, int reclen, char *rec)
 	int col = getshort(rec, 2);
 	int string_no = getshort(rec, 6);
 
+#ifdef EDEBUG
+	fprintf(stderr, "Got CONSTANT_STRING, row=%d, col=%d\n", row, col);
+#endif
 	saved_reference = NULL;
-	allocate(row, col);
+	if (allocate(row, col)) return 1;
 	prow = rowptr + row;
 	if (string_no >= sstsize) {
 	    fprintf(stderr, "string index too large\n");
@@ -180,10 +259,8 @@ static void process_item (int rectype, int reclen, char *rec)
 	    char *outptr;
 
 	    outptr = prow->cells[col] = malloc(len + 2);
-	    fprintf(stderr, "malloced %d bytes at %p:", len + 2, (void *) outptr);
 	    *(outptr++) = '"';
 	    strcpy(outptr, sst[string_no]);
-	    fprintf(stderr, " '%s'\n", outptr);
 	} else {
 	    prow->cells[col] = malloc(1);
 	    strcpy(prow->cells[col], "");
@@ -197,9 +274,38 @@ static void process_item (int rectype, int reclen, char *rec)
 	saved_reference = NULL;
 	row = getshort(rec, 0) - startrow; 
 	col = getshort(rec, 2);
-	allocate(row, col);
+#ifdef EDEBUG
+	fprintf(stderr, "Got NUMBER, row=%d, col=%d\n", row, col);
+#endif
+	if (allocate(row, col)) return 1;
 	prow = rowptr + row;
-	prow->cells[col] = strdup(format_double(rec, 6));
+	prow->cells[col] = g_strdup(format_double(rec, 6));
+	break;
+    }
+    case MULRK: {
+	int i, row, col, ncols;
+	double v;
+	struct rowdescr *prow;
+
+	saved_reference = NULL;
+	row = getshort(rec, 0) - startrow; 
+	col = getshort(rec, 2);
+	ncols = (reclen - 6)/ 6;
+#ifdef EDEBUG
+	fprintf(stderr, "Got MULRK, row=%d, first_col=%d, sz=%d, ", 
+		row, col, reclen);
+	fprintf(stderr, "ncols = %d\n", ncols);
+#endif
+	for (i=0; i<ncols; i++) {
+	    char tmp[32];
+
+	    v = biff_get_rk(rec + 6 + 6*i);
+	    if (allocate(row, col)) return 1;
+	    prow = rowptr + row;
+	    sprintf(tmp, "%.10g", v);
+	    prow->cells[col] = g_strdup(tmp);
+	    col++;
+	}
 	break;
     }
     case FORMULA: { 
@@ -209,7 +315,10 @@ static void process_item (int rectype, int reclen, char *rec)
 	saved_reference = NULL;
 	row = getshort(rec, 0) - startrow; 
 	col = getshort(rec, 2);
-	allocate(row, col);
+#ifdef EDEBUG
+	fprintf(stderr, "Got FORMULA, row=%d, col=%d\n", row, col);
+#endif
+	if (allocate(row, col)) return 1;
 	prow = rowptr + row;
 	if (((unsigned char) rec[12] == 0xFF) && 
 	    (unsigned char) rec[13] == 0xFF) {
@@ -219,18 +328,17 @@ static void process_item (int rectype, int reclen, char *rec)
 		char buf[2] = "0";
 
 		buf[0] += rec[9];
-		prow->cells[col] = strdup(buf);
+		prow->cells[col] = g_strdup(buf);
 	    } else if (rec[6] == 2) {
 		/* error */
 		char buf[6] = "ERROR";
 
-		prow->cells[col] = strdup(buf);
+		prow->cells[col] = g_strdup(buf);
 	    } else if (rec[6] == 0) {
 		saved_reference = prow->cells + col;
 	    }   
-	} else {
-	    prow->cells[col] = strdup(format_double(rec, 6));
-	}		 
+	} else 
+	    prow->cells[col] = g_strdup(format_double(rec, 6));
 	break;
     }
     case STRING: {
@@ -241,7 +349,7 @@ static void process_item (int rectype, int reclen, char *rec)
 	    break;
 	}
 	len = getshort(rec, 0);
-	*saved_reference = mark_string(convert8to8(rec + 2, len + 1));
+	*saved_reference = mark_string(convert8to7(rec + 2, len + 1));
 	break;
     }	    
     case BOF: {
@@ -260,12 +368,15 @@ static void process_item (int rectype, int reclen, char *rec)
     default: 
 	break;
     }
+    return 0;
 }  
 
 static char *mark_string (char *instr) 
 {
     int len = strlen(instr);
     char *out = malloc(len+2);
+
+    if (out == NULL) return NULL;
 
     *out = '"';
     strcpy(out+1, instr);
@@ -293,7 +404,7 @@ static char *copy_unicode_string (char **src)
 	fprintf(stderr, "Extended string found length=%d charsize=%d "
 		"%d bytes to skip\n", count, charsize, to_skip);
 	*src += to_skip;
-	return strdup("Extended string");
+	return g_strdup("Extended string");
     } else {
 	to_skip = 3 + charsize * count + 
 	    ((flags & 0x8)? (2 + 4 * getshort(*src, 3)) : 0);
@@ -301,30 +412,38 @@ static char *copy_unicode_string (char **src)
     realstart = *src + ((flags & 0x8)? 6 : 3);
     *src += to_skip;
     if (charsize == 1) 
-	return convert8to8(realstart, count);
+	return convert8to7(realstart, count);
     else 
-	return convert16to8(realstart, count);
+	return convert16to7(realstart, count);
 }
 
-static char *convert8to8 (char *src, int count) 
+static char *convert8to7 (char *src, int count) 
 {
     char *dest = malloc(count + 1);
     char *s, *d;
     int c, i, u;
+
+    if (dest == NULL) return NULL;
 
     *dest = 0;
     for (s=src, d=dest, i=0; i<count; i++, s++) {
 	u = (unsigned char) *s;
 	c = (u < 128)? u : '_';
 	*d++ = c;
-    }   
+    }
+#ifdef EDEBUG
+    fprintf(stderr, "convert8to7: returning '%s' at %p\n", dest,
+	    (void *) dest);
+#endif    
     return dest;
 }
 
-static char *convert16to8 (char *src, int count) 
+static char *convert16to7 (char *src, int count) 
 {
     char *s, *dest = malloc(count + 1);
     int c, i, j, u;
+
+    if (dest == NULL) return NULL;
 
     for (s=src, i=0, j=0; i<count; i++, s+=2) {
 	u = getshort(s, 0);
@@ -334,54 +453,57 @@ static char *convert16to8 (char *src, int count)
     return dest;
 }
 
-static void allocate (int row, int col) 
+static int allocate (int row, int col) 
 {
     int newrow, newcol;
 
     if (row >= lastrow) {
 	newrow = (row/16 + 1) * 16;
 	rowptr = realloc(rowptr, newrow * sizeof(struct rowdescr));
+	if (rowptr == NULL) return 1;
 	memset(rowptr + lastrow, 0, (newrow-lastrow) * sizeof(struct rowdescr));
 	lastrow = newrow;
     }
     if (col >= rowptr[row].end) {
 	newcol = (col/16 + 1) * 16;
 	rowptr[row].cells = realloc(rowptr[row].cells, newcol * sizeof(char *));
+	if (rowptr[row].cells == NULL) return 1;
 	memset(rowptr[row].cells + rowptr[row].end, 0, (newcol-rowptr[row].end)
 	       *sizeof(char *));
 	rowptr[row].end = newcol;
     }  
     if (col > rowptr[row].last) rowptr[row].last = col;
+    return 0;
 }
 
 static int getshort (char *rec, int offset) 
 {
     return (signed short int) (*((unsigned char *)(rec + offset)) |
-			       ((*((unsigned char *)(rec + offset + 1)))<<8));
+			       ((*((unsigned char *)(rec + offset + 1))) << 8));
 }	      
 
 static void free_sheet (void) 
 {
     int i, j;
-    struct rowdescr *row;
-    char **col;
 
     printf("free_sheet(), lastrow=%d\n", lastrow);
-    
-    /* was i<lastrow below */
-    for (i=0, row=rowptr; i<=lastrow; i++, row++) {
-	if (row->cells == NULL) continue;
-#ifdef notdef
-	for (j=0, col=row->cells; j<row->end; j++, col++) 
-	    if (*col) free(*col);
-#endif
-	for (j=0, col=row->cells; j<=row->last; j++, col++) 
-	    if (*col) free(*col);
-	fprintf(stderr, "freeing %p\n", (void *) row->cells);
-	free(row->cells);
-    }
-    free(rowptr);
+
+    /* free shared string table */
+    for (i=0; i<sstsize; i++) 
+	free(sst[i]);
     free(sst);
+
+    /* free cells (was i<lastrow below) */
+    for (i=0; i<=lastrow; i++) {
+	if (rowptr[i].cells == NULL) continue;
+	for (j=0; j<rowptr[i].end; j++) {
+	    if (rowptr[i].cells[j] == NULL) continue;
+	    free(rowptr[i].cells[j]);
+	}
+	free(rowptr[i].cells);
+    }
+
+    free(rowptr);
     rowptr = NULL;
     lastrow = 0;
 }
