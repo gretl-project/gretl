e9a6dbbee48abfb137004a5abd4cda5127073825
Allin Cottrell <cottrell@wfu.edu>
2003-03-31 15:04:06 +0000

    raise lib revision number

--- lib/src/version.h
+++ lib/src/version.h
@@ -21,7 +21,7 @@
 #define GRETL_VERSION_H
 
 #define LIBGRETL_CURRENT   0
-#define LIBGRETL_REVISION  5
+#define LIBGRETL_REVISION  6
 #define LIBGRETL_AGE       0
 
 const char *version_string = "1.0.7";
