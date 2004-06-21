b5de54c572d176a8a30fdd628a27af9432de2135
Allin Cottrell <cottrell@wfu.edu>
2004-06-21 18:44:53 +0000

    update gtk-1.2 version

--- gui/gretl.c
+++ gui/gretl.c
@@ -1615,7 +1615,9 @@ static void startR (gpointer p, guint opt, GtkWidget *w)
     }
 
     if (dataset_is_time_series(datainfo)) {
-	fprintf(fp, "source(\"%s\", echo=TRUE)\n", Rdata);
+        fputs("vnum <- as.double(R.version$major) + (as.double(R.version$minor) / 10.0)\n", fp);
+        fputs("if (vnum > 1.89) library(stats) else library(ts)\n", fp);
+        fprintf(fp, "source(\"%s\", echo=TRUE)\n", Rdata);
     } else {
 	char Rtmp[MAXLEN];
 	FILE *fq;
