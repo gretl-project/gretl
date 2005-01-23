29e91beb4b058fc6de6061c12a2ddcb0ebe14d8e
Allin Cottrell <cottrell@wfu.edu>
2005-01-23 23:21:49 +0000

    *** empty log message ***

--- win32/config.mk
+++ win32/config.mk
@@ -1,19 +1,19 @@
 # Configuration variables governing the build of gretl for win32
 
 # directory containing the cross tools
-CROSSDIR = /opt/cross-tools
+CROSSDIR = /opt/cross-tools/mingw32
 
 # prefix for mingw tools (e.g. mingw32-gcc)
 MGW_PREFIX = mingw32
 
 # mingw include dir
-MGW_INC = $(CROSSDIR)/mingw32/include
+MGW_INC = $(CROSSDIR)/include
 
 # glib includes: adjust to match your system
-GLIB_INC = $(CROSSDIR)/mingw32/include/glib-2.0
+GLIB_INC = $(CROSSDIR)/include/glib-2.0
 
 # msgfmt command for producing win32 messages file
 WIN32_MSGFMT = wine c:/bin/msgfmt.exe
 
 # pkgconfig path
-PKG_CONFIG_PATH = $(CROSSDIR)/mingw32/lib/pkgconfig
+PKG_CONFIG_PATH = $(CROSSDIR)/lib/pkgconfig
