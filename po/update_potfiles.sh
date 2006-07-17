f4b648cde7dd059539fb5039bc99e27dd79ff3ac
Allin Cottrell <cottrell@wfu.edu>
2006-07-17 18:31:50 +0000

    prune gui out of potfile search

--- po/update_potfiles.sh
+++ po/update_potfiles.sh
@@ -10,10 +10,6 @@ for f in ../cli/*.c ; do
    echo $f | sed s+\.\./++ >> POTFILES.in
 done   
 
-for f in `find ../gui -maxdepth 1 -name "*.c" -type f` ; do
-   echo $f | sed s+\.\./++ >> POTFILES.in
-done 
-
 for f in ../gui2/*.c ; do
    echo $f | sed s+\.\./++ >> POTFILES.in
 done
