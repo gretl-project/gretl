bdb730f965cb35bf88449a43f09f273c6cda8fed
Allin Cottrell <cottrell@wfu.edu>
2007-02-24 02:43:28 +0000

    update version number

--- lib/src/version.h
+++ lib/src/version.h
@@ -21,9 +21,9 @@
 #define GRETL_VERSION_H
 
 #define LIBGRETL_CURRENT   0
-#define LIBGRETL_REVISION  33
+#define LIBGRETL_REVISION  34
 #define LIBGRETL_AGE       0
 
-#define GRETL_VERSION "1.6.0"
+#define GRETL_VERSION "1.6.1"
 
 #endif
