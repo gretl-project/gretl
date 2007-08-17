5a484a5bd9d88e713f740e70d90bd57fa86c87c3
Allin Cottrell <cottrell@wfu.edu>
2007-08-17 18:35:41 +0000

    add experimental std errs correction for some VECM cases

--- plugin/johansen.c
+++ plugin/johansen.c
@@ -532,7 +532,8 @@ static int vecm_check_size (GRETL_VAR *v, int flags)
 /* For estimating both alpha and Gamma: add the EC terms into
    the X data matrix */
 
-static int add_EC_terms_to_X (GRETL_VAR *v, const double **Z)
+static int add_EC_terms_to_X (GRETL_VAR *v, gretl_matrix *X,
+			      const double **Z)
 {
     const gretl_matrix *B = v->jinfo->Beta;
     int rank = jrank(v);
@@ -561,7 +562,7 @@ static int add_EC_terms_to_X (GRETL_VAR *v, const double **Z)
 		bxt += bij;
 	    }
 
-	    gretl_matrix_set(v->X, s, k, bxt);
+	    gretl_matrix_set(X, s, k, bxt);
 	}
     }
 	
@@ -575,7 +576,7 @@ static int make_vecm_Y (GRETL_VAR *v, const double **Z,
 			gretl_matrix *Pi, int flags)
 {
     int i, s, t, vi, vj;
-    double yti, xti;
+    double pij, yti, xti;
     int err = 0;
 
     if (flags & ESTIMATE_ALPHA) {
@@ -595,28 +596,137 @@ static int make_vecm_Y (GRETL_VAR *v, const double **Z,
 	form_Pi(v, Pi);
 
 	for (i=0; i<v->neqns; i++) {
+	    int wexo = 1;
+
 	    vi = v->ylist[i+1];
 	    s = 0;
 	    for (t=v->t1; t<=v->t2; t++) {
 		/* first difference */
 		yti = Z[vi][t] - Z[vi][t-1];
 		for (j=0; j<p1; j++) {
-		    if (j < v->neqns) {
-			/* lagged level */
-			vj = v->ylist[j+1];
-			xti = Z[vj][t-1];
-		    } else {
-			xti = (jcode(v) == J_REST_TREND)? t : 1;
-		    } 
-		    yti -= gretl_matrix_get(Pi, i, j) * xti;
+		    pij = gretl_matrix_get(Pi, i, j);
+		    if (pij != 0.0) {
+			if (j < v->neqns) {
+			    /* lagged level */
+			    wexo = 0;
+			    vj = v->ylist[j+1];
+			    xti = Z[vj][t-1];
+			} else {
+			    xti = (jcode(v) == J_REST_TREND)? t : 1;
+			} 
+			yti -= pij * xti;
+		    }
 		}
 		gretl_matrix_set(v->Y, s++, i, yti);
+	    }
+
+	    if (wexo) {
+		fprintf(stderr, "make_vecm_Y: var %d is weakly exogenous\n", i);
 	    }
 	}
     } 
 
     return err;
 }
+
+#if 1
+
+static int eqn_is_unrestricted (const gretl_matrix *R, int j)
+{
+    int i, rest = 0;
+
+    for (i=0; i<R->rows; i++) {
+	if (gretl_matrix_get(R, i, j) != 0.0) {
+	    rest = 1;
+	    break;
+	}
+    }
+
+    return !rest;
+}
+
+/* (Experimental) apparatus for getting correct (?) standard errors
+   for Gamma etc., in case we've estimated the models netting out
+   \Pi*X^*_{t-1}.  At present we attempt a correction only for
+   equations where the \alpha estimates are unrestricted; we recompute
+   the std errors using the full X'X matrix, including the
+   \beta'X^*_{t-1} column(s).  Is this right?
+   
+   Note: if the dependent variable in a given equation is weakly
+   exogenous, then netting out \Pi*X^*_{t-1} should be OK, with regard
+   to the standard errors for Gamma etc., shouldn't it?  Because in
+   that case the "missing" \beta'X^*_{t-1} column(s) are all zeros.
+*/
+
+static int 
+correct_variance (GRETL_VAR *v, const gretl_restriction *rset,
+		  const double **Z)
+{
+    const gretl_matrix *R;
+    gretl_matrix *X = NULL;
+    gretl_matrix *XTX = NULL;
+    double x;
+    int xc1, nse;
+    int i, j, err = 0;
+
+    R = rset_get_Ra_matrix(rset);
+    if (R == NULL) {
+	/* shouldn't be here */
+	return 0;
+    }
+
+#if JDEBUG
+    gretl_matrix_print(R, "Ra, in correct_variance()");
+#endif
+
+    nse = v->X->cols;
+    xc1 = v->X->cols + jrank(v);
+
+    X = gretl_zero_matrix_new(v->T, xc1);
+    XTX = gretl_matrix_alloc(xc1, xc1);
+
+    if (X == NULL || XTX == NULL) {
+	err = E_ALLOC;
+    }
+
+    if (!err) {
+	/* copy original X cols and append ECs */
+	gretl_matrix_inscribe_matrix(X, v->X, 0, 0, GRETL_MOD_NONE);
+	err = add_EC_terms_to_X(v, X, Z);
+    }
+
+    if (!err) {
+	/* form X'X and invert */
+	gretl_matrix_multiply_mod(X, GRETL_MOD_TRANSPOSE,
+				  X, GRETL_MOD_NONE,
+				  XTX, GRETL_MOD_NONE);
+	gretl_invert_symmetric_matrix(XTX);
+    }	
+
+    for (i=0; i<v->neqns && !err; i++) {
+	MODEL *pmod = v->models[i];
+	
+	if (eqn_is_unrestricted(R, i)) {
+	    for (j=0; j<nse; j++) {
+		x = gretl_matrix_get(XTX, j, j);
+		x = sqrt(x);
+		x *= pmod->sigma;
+#if JDEBUG
+		fprintf(stderr, "original se[%d,%d] = %g\n", i, j, pmod->sderr[j]);
+		fprintf(stderr, " revised se[%d,%d] = %g\n", i, j, x);
+#endif
+		pmod->sderr[j] = x;
+	    }
+	}
+    }
+
+    gretl_matrix_free(X);
+    gretl_matrix_free(XTX);
+
+    return err;
+}
+
+#endif
 
 /* The following is designed to accommodate the case where alpha is
    restricted, so we can't just run OLS conditional on beta.
@@ -632,7 +742,8 @@ static int make_vecm_Y (GRETL_VAR *v, const double **Z,
 */
 
 static int 
-VECM_estimate_full (GRETL_VAR *v, const double **Z, const DATAINFO *pdinfo,
+VECM_estimate_full (GRETL_VAR *v, const gretl_restriction *rset,
+		    const double **Z, const DATAINFO *pdinfo,
 		    int flags)
 {
     gretl_matrix *Pi = NULL;
@@ -676,7 +787,7 @@ VECM_estimate_full (GRETL_VAR *v, const double **Z, const DATAINFO *pdinfo,
     }
 
     if (!err && (flags & ESTIMATE_ALPHA)) {
-	err = add_EC_terms_to_X(v, Z);
+	err = add_EC_terms_to_X(v, v->X, Z);
     }
 
     if (!err) {
@@ -747,6 +858,13 @@ VECM_estimate_full (GRETL_VAR *v, const double **Z, const DATAINFO *pdinfo,
 	    transcribe_alpha(v);
 	}
     }
+
+#if 1
+    /* experimental */
+    if (!err &&  flags == NET_OUT_ALPHA) {
+	err = correct_variance(v, rset, Z);
+    }
+#endif
 
  bailout:
     
@@ -1488,7 +1606,7 @@ static int j_general_restrict (GRETL_VAR *jvar,
     if (!err) {
 	int flag = (acols > 0)? NET_OUT_ALPHA : ESTIMATE_ALPHA;
 
-	err = VECM_estimate_full(jvar, Z, pdinfo, flag);
+	err = VECM_estimate_full(jvar, rset, Z, pdinfo, flag);
     }
 
     if (!err) {
@@ -1542,7 +1660,7 @@ static int vecm_finalize (GRETL_VAR *jvar, gretl_matrix *H,
     err = normalize_beta(jvar, H, &do_stderrs); 
 
     if (!err) {
-	err = VECM_estimate_full(jvar, Z, pdinfo, ESTIMATE_ALPHA);
+	err = VECM_estimate_full(jvar, NULL, Z, pdinfo, ESTIMATE_ALPHA);
     }
 
     if (!err) {
@@ -1780,7 +1898,7 @@ johansen_boot_round (GRETL_VAR *jvar, const double **Z,
 	gretl_matrix_copy_values(jvar->jinfo->Beta, M);
 	err = normalize_beta(jvar, NULL, NULL); 
 	if (!err) {
-	    err = VECM_estimate_full(jvar, Z, pdinfo, 
+	    err = VECM_estimate_full(jvar, NULL, Z, pdinfo, 
 				     ESTIMATE_ALPHA | BOOTSTRAPPING);
 	}
 	if (!err) {
