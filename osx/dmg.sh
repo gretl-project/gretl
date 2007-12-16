b7389ab43f1e553fc342d0e58b90c092fc044786
Allin Cottrell <cottrell@wfu.edu>
2007-12-16 16:56:49 +0000

    *** empty log message ***

--- osx/dmg.sh
+++ osx/dmg.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# the directory above Gretl.app
+TOPDIR=/Users/allin/dist
+
 HERE=`pwd`
 KB=`du -ks ~/dist | awk '{ print $1 }'`
 KB=$((KB+640))
@@ -8,7 +11,7 @@ MYDEV=`hdid -nomount tmp.dmg`
 sudo newfs_hfs -v gretl $MYDEV
 hdiutil eject $MYDEV
 hdid tmp.dmg
-cd ~/dist && \
+cd $TOPDIR && \
 cp -a Gretl.app /Volumes/gretl && \
 cp -a README.pdf /Volumes/gretl
 cd $HERE
