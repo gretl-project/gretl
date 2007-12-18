e38bb75b01519b6f95eb8f3caa42d7a91f99eca8
Allin Cottrell <cottrell@wfu.edu>
2007-12-18 23:14:07 +0000

    *** empty log message ***

--- osx/dmg.sh
+++ osx/dmg.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # The directory above Gretl.app
-TOPDIR=/Users/allin/dist
+TOPDIR="/Users/allin/dist"
 
 rm -f gretl.dmg tmp.dmg
 
 HERE=`pwd`
-KB=`du -ks $TOPDIR | awk '{ print $1 }'`
+KB=`du -ks ${TOPDIR} | awk '{ print $1 }'`
 KB=$((KB+1024))
 hdiutil create -size ${KB}k tmp.dmg -layout NONE
 MYDEV=`hdid -nomount tmp.dmg`
 sudo newfs_hfs -v gretl $MYDEV
 hdiutil eject $MYDEV
 hdid tmp.dmg
-cd $TOPDIR && \
+cd ${TOPDIR} && \
 /sw/bin/cp -a Gretl.app /Volumes/gretl && \
 /sw/bin/cp -a README.pdf /Volumes/gretl
 cd $HERE
