21c20139f9516c1efeda786b8d4a6e56bde7b023
Allin Cottrell <cottrell@wfu.edu>
2009-02-25 16:09:19 +0000

    binomial plot: try using an alternative computational formula

--- gui2/calculator.c
+++ gui2/calculator.c
@@ -256,9 +256,15 @@ enum {
                  "x**(0.5*m-1.0)/(1.0+m/n*x)**(0.5*(m+n))"
 #define F_STUD   "stud(x,m)=Binv(0.5*m,0.5)/sqrt(m)*(1.0+(x*x)/m)**(-0.5*(m+1.0))"
 #define F_COMB   "comb(n,k)=n!/(k!*(n-k)!)"
-#define F_BINOM  "binom(k,n,p)=comb(int(n),int(k))*p**k*(1-p)**(n-k)"
 #define F_POIS   "poisson(z,k)=exp(-z)*(z**k)/(int(k))!"
 #define F_WEIB   "weibull(x,shp,scl)=(shp/scl)*(x/scl)**(shp-1.0)*exp(-(x/scl)**shp)"
+
+#if 1
+#define F_BINOM  "binom(k,n,p)=k<0||k>n?0.0:exp(lgamma(n+1)-lgamma(n-k+1)-lgamma(k+1)" \
+                 "+k*log(p)+(n-k)*log(1.0-p))"
+#else
+#define F_BINOM  "binom(k,n,p)=comb(int(n),int(k))*p**k*(1-p)**(n-k)"
+#endif
 
 static void 
 dist_xmin_xmax (int d, double *parms, double *xmin, double *xmax,
