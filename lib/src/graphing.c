6fec29e7bd91d121dafb227160c08b0e437eca9d
Allin Cottrell <cottrell@wfu.edu>
2009-04-27 05:53:26 +0000

    prefer pngcairo in gnuplot

--- lib/src/graphing.c
+++ lib/src/graphing.c
@@ -427,7 +427,10 @@ int gnuplot_has_ttf (int reset)
        with X11 */
 
     if (err == -1 || reset) {
-	err = gnuplot_test_command("set term png font Vera 8");
+        err = gnuplot_test_command("set term pngcairo");
+        if (err) {
+	    err = gnuplot_test_command("set term png font Vera 8");
+        }
 	if (err) {
 	    err = gnuplot_test_command("set term png font luxisr 8");
 	}
