58220be3088ecb5023c75794fc177cc4f420d308
Allin Cottrell <cottrell@wfu.edu>
2009-05-18 00:53:09 +0000

    bump version info for 1.8.1

--- lib/src/version.h
+++ lib/src/version.h
@@ -21,9 +21,9 @@
 #define GRTL_VERSION_H
 
 #define LIBGRETL_CURRENT   0
-#define LIBGRETL_REVISION  50
+#define LIBGRETL_REVISION  51
 #define LIBGRETL_AGE       0
 
-#define GRETL_VERSION "1.8.0cvs"
+#define GRETL_VERSION "1.8.1"
 
 #endif /* GRTL_VERSION_H */
