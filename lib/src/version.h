759eeec2dfdc192e7c97f331132b5b48bbf82bae
Allin Cottrell <cottrell@wfu.edu>
2009-08-11 20:33:23 +0000

    another doc update ; bump version for release

--- lib/src/version.h
+++ lib/src/version.h
@@ -21,9 +21,9 @@
 #define GRETL__VERSION_H
 
 #define LIBGRETL_CURRENT   0
-#define LIBGRETL_REVISION  52
+#define LIBGRETL_REVISION  53
 #define LIBGRETL_AGE       0
 
-#define GRETL_VERSION "1.8.2cvs"
+#define GRETL_VERSION "1.8.3"
 
 #endif /* GRETL__VERSION_H */
