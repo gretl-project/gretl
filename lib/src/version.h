e66cd9234871b6fc1b2122d1b61b1fc99e47efea
Allin Cottrell <cottrell@wfu.edu>
2009-11-25 15:31:24 +0000

    get ready for 1.8.6 release

--- lib/src/version.h
+++ lib/src/version.h
@@ -21,9 +21,9 @@
 #define GRETL__VERSION_H
 
 #define LIBGRETL_CURRENT   0
-#define LIBGRETL_REVISION  55
+#define LIBGRETL_REVISION  56
 #define LIBGRETL_AGE       0
 
-#define GRETL_VERSION "1.8.5cvs"
+#define GRETL_VERSION "1.8.6"
 
 #endif /* GRETL__VERSION_H */
