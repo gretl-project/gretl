41e9c4a70efa274794417a97975f6ed1cae02ef8
Allin Cottrell <cottrell@wfu.edu>
2010-07-10 17:55:56 +0000

    rename accessor mln_prob -> mnlprobs (Jack was right!)

--- lib/src/genlex.c
+++ lib/src/genlex.c
@@ -136,7 +136,7 @@ struct str_table mvars[] = {
     { M_COEFF_CI,"$coeff_ci" },
     { M_KLLT,    "$kalman_llt" },
     { M_KUHAT,   "$kalman_uhat" },
-    { M_MNLPROBS, "$mnl_prob" },
+    { M_MNLPROBS, "$mnlprobs" },
     { M_XLIST,   "$xlist" },
     { 0,         NULL }
 };
