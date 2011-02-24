f3e2841c9f513b057a7385e5843bd6a1100f0f81
Allin Cottrell <cottrell@wfu.edu>
2011-02-24 19:45:14 +0000

    version to 1.9.4

--- lib/src/version.h
+++ lib/src/version.h
@@ -21,9 +21,9 @@
 #define GRETL__VERSION_H
 
 #define LIBGRETL_CURRENT   0
-#define LIBGRETL_REVISION  61
+#define LIBGRETL_REVISION  62
 #define LIBGRETL_AGE       0
 
-#define GRETL_VERSION "1.9.3cvs"
+#define GRETL_VERSION "1.9.4"
 
 #endif /* GRETL__VERSION_H */
