0add08036aa600d34e575b976d56c19c245d9496
Allin Cottrell <cottrell@wfu.edu>
2011-04-22 18:06:31 +0000

    bump version for 1.9.5

--- lib/src/version.h
+++ lib/src/version.h
@@ -21,9 +21,9 @@
 #define GRETL__VERSION_H
 
 #define LIBGRETL_CURRENT   0
-#define LIBGRETL_REVISION  62
+#define LIBGRETL_REVISION  63
 #define LIBGRETL_AGE       0
 
-#define GRETL_VERSION "1.9.4cvs"
+#define GRETL_VERSION "1.9.5"
 
 #endif /* GRETL__VERSION_H */
