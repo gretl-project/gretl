08358614464d2ee4fb13003dc54ec8c3f25bb15a
Allin Cottrell <cottrell@wfu.edu>
2011-04-28 16:39:15 +0000

    ad translatable strings for addons

---
+++ plugin/addons-i18n.c
@@ -0,0 +1,41 @@
+/* translatable strings for gretl addons */
+
+const char *gig_translations[] = {
+    N_("gig"),
+    N_("APARCH"),
+    N_("ARCH"),
+    N_("Covariance estimator"),
+    N_("Density"),
+    N_("Dependent Variable"),
+    N_("Distribution"),
+    N_("EGARCH"),
+    N_("GARCH"),
+    N_("GED"),
+    N_("GJR"),
+    N_("Hessian"),
+    N_("Mean regressors"),
+    N_("Model type"),
+    N_("NARCH"),
+    N_("Normal"),
+    N_("OPG"),
+    N_("Plot type"),
+    N_("Sandwich"),
+    N_("Skewed GED"),
+    N_("Skewed t"),
+    N_("TARCH"),
+    N_("Taylor/Schwert GARCH"),
+    N_("Time series"),
+    N_("Variance regressors"),
+    N_("Verbosity"),
+    N_("t")
+};
+
+const char *ivpanel_translations[] = {
+    N_("Between model"),
+    N_("Fixed effects"),
+    N_("G2SLS"),
+    N_("dependent variable"),
+    N_("instruments"),
+    N_("regressors")
+};
+
