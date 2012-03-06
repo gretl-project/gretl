b8329c2b22345acf5e5d041186bd816159399de7
Allin Cottrell <cottrell@wfu.edu>
2012-03-06 02:43:00 +0000

    *** empty log message ***

--- gui2/gui_utils.c
+++ gui2/gui_utils.c
@@ -897,7 +897,7 @@ int get_imported_data (char *fname, int ftype, int append)
     } else if (ftype == GRETL_XLSX) {
 	ss_importer = gui_get_plugin_function("xlsx_get_data",
 					      &handle);
-	list[1] = 0;
+	list[1] = 0; /* XLSX sheet filename issue */
 	plist = list;
     } else if (ftype == GRETL_GNUMERIC) {
 	ss_importer = gui_get_plugin_function("gnumeric_get_data",
