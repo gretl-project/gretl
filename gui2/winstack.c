34fe735959c8ddb9aab24626bc3e738015ff6dd0
Allin Cottrell <cottrell@wfu.edu>
2012-04-05 13:04:26 +0000

    *** empty log message ***

--- gui2/winstack.c
+++ gui2/winstack.c
@@ -916,9 +916,10 @@ static void menu_bar_add_winlist (windata_t *vwin)
 {
     GtkWidget *img, *button = gtk_button_new();
     GtkWidget *hbox = gtk_widget_get_parent(vwin->mbar);
-    GtkStyle *style = gtk_widget_get_style(vwin->mbar);
 
 #if GTK_MAJOR_VERSION > 2 || GTK_MINOR_VERSION >= 16
+    /* doesn't work with gtk 3.2.4 */
+    GtkStyle *style = gtk_widget_get_style(vwin->mbar);
     GValue val = {0};
 
     g_value_init(&val, G_TYPE_INT);
