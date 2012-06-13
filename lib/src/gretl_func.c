e2ea08bf333626d4189b831f78b8f1a21cefc341
Riccardo "Jack" Lucchetti <r.lucchetti@univpm.it>
2012-06-13 07:38:58 +0000

    allow "null" as a list argument in functions

--- lib/src/gretl_func.c
+++ lib/src/gretl_func.c
@@ -6305,6 +6305,8 @@ static int check_function_args (ufunc *u, fnargs *args, PRN *prn)
 	    ; /* OK */
 	} else if (fp->type == GRETL_TYPE_LIST && arg->type == GRETL_TYPE_USERIES) {
 	    ; /* OK */
+	} else if (fp->type == GRETL_TYPE_LIST && arg->type == GRETL_TYPE_NONE) {
+	    ; /* OK */
 	} else if (fp->type != arg->type) {
 	    pprintf(prn, _("%s: argument %d is of the wrong type (is %s, should be %s)\n"), 
 		    u->name, i + 1, gretl_arg_type_name(arg->type), 
