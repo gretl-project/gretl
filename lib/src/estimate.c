97b9d08c56bd2a19638c0e9cba3616d344b04c04
Allin Cottrell <cottrell@wfu.edu>
2014-01-10 19:15:15 +0000

    ensure that a constant is included in White's test aux regression

--- lib/src/estimate.c
+++ lib/src/estimate.c
@@ -3053,6 +3053,49 @@ static double get_BP_LM (MODEL *pmod, int *list, MODEL *aux,
     return LM;
 }
 
+static int *ensure_const_copy_list (const MODEL *pmod, 
+				    DATASET *dset,
+				    int *err)
+{
+    int cpos = gretl_list_const_pos(pmod->list, 2, dset);    
+    int *list = NULL;
+
+    if (cpos > 0) {
+	list = gretl_list_copy(pmod->list);
+    } else {
+	int i, nl = pmod->list[0] + 1;
+	
+	list = gretl_list_new(nl);
+	if (list != NULL) {
+	    list[1] = pmod->list[1];
+	    list[2] = 0; /* add const */
+	    for (i=3; i<=nl; i++) {
+		list[i] = pmod->list[i-1];
+	    }
+	}
+    }
+
+    if (list == NULL) {
+	*err = E_ALLOC;
+    }
+
+    return list;
+}
+
+static int *ensure_const_augment_list (const MODEL *pmod, int aux,
+				       DATASET *dset, int *err)
+{
+    int *tmp = ensure_const_copy_list(pmod, dset, err);
+    int *list = NULL;
+
+    if (!*err) {
+	list = augment_regression_list(tmp, aux, dset, err);
+	free(tmp);
+    }
+
+    return list;
+}
+
 /**
  * whites_test:
  * @pmod: pointer to model.
@@ -3136,12 +3179,12 @@ int whites_test (MODEL *pmod, DATASET *dset,
 
     if (!err) {
 	if (BP) {
-	    list = gretl_list_copy(pmod->list);
+	    list = ensure_const_copy_list(pmod, dset, &err);
 	} else {
 	    /* build aux regression list, adding squares and
 	       (possibly) cross-products of the original 
 	       independent vars */
-	    list = augment_regression_list(pmod->list, aux, dset, &err);
+	    list = ensure_const_augment_list(pmod, aux, dset, &err);
 	}
 	if (!err){
 	    list[1] = v; /* the newly added uhat-squared */
