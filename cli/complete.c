3ce1b77b61c82c849c55ff3d72700dfc871a16c7
Allin Cottrell <cottrell@wfu.edu>
2014-03-08 19:29:10 +0000

    update for readline 6.3 (check that this works with rl <= 6.2!)

--- cli/complete.c
+++ cli/complete.c
@@ -126,6 +126,6 @@ void initialize_readline (void)
     rl_readline_name = "gretl";
 
     /* Tell the completer that we want a crack first. */
-    rl_attempted_completion_function = (CPPFunction *) gretl_completion;
+    rl_attempted_completion_function = (rl_completion_func_t *) gretl_completion;
 }
 
