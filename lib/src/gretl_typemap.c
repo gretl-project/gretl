e993e63e05a15cd9532b12cc321d43a1836482cf
Allin Cottrell <cottrell@wfu.edu>
2014-06-18 16:44:26 +0000

    add a new source file for handling type mappings

---
+++ lib/src/gretl_typemap.c
@@ -0,0 +1,183 @@
+/* 
+ *  gretl -- Gnu Regression, Econometrics and Time-series Library
+ *  Copyright (C) 2001 Allin Cottrell and Riccardo "Jack" Lucchetti
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#include "libgretl.h"
+#include "gretl_typemap.h"
+
+struct {
+    GretlType sing;
+    GretlType sref;
+    GretlType plural;
+    GretlType pref;
+} type_map;
+
+static struct type_map gretl_type_map[] = {
+    { GRETL_TYPE_MATRIX,   GRETL_TYPE_MATRIX_REF, 
+      GRETL_TYPE_MATRICES, GRETL_TYPE_MATRICES_REF},
+    { GRETL_TYPE_BUNDLE,   GRETL_TYPE_BUNDLE_REF, 
+      GRETL_TYPE_BUNDLES,  GRETL_TYPE_BUNDLES_REF},
+    { GRETL_TYPE_STRING,   0,
+      GRETL_TYPE_STRINGS,  GRETL_TYPE_STRINGS_REF},
+    { GRETL_TYPE_LIST,     0, 
+      GRETL_TYPE_LISTS,    GRETL_TYPE_LISTS_REF}
+};
+
+GretlType gretl_type_get_plural (GretlType type)
+{
+    int i, n = G_N_ELEMENTS(gretl_type_map);
+
+    for (i=0; i<n; i++) {
+	if (type == gretl_type_map[i].sing) {
+	    return gretl_type_map[i].plural;
+	}
+    }
+
+    return 0;
+}
+
+GretlType gretl_type_get_singular (GretlType type)
+{
+    int i, n = G_N_ELEMENTS(gretl_type_map);
+
+    for (i=0; i<n; i++) {
+	if (type == gretl_type_map[i].plural) {
+	    return gretl_type_map[i].sing;
+	}
+    }
+
+    return 0;
+}
+
+GretlType gretl_type_get_ref_type (GretlType type)
+{
+    int i, n = G_N_ELEMENTS(gretl_type_map);
+
+    for (i=0; i<n; i++) {
+	if (type == gretl_type_map[i].sing) {
+	    return gretl_type_map[i].sref;
+	}
+	if (type == gretl_type_map[i].plural) {
+	    return gretl_type_map[i].pref;
+	}	
+    }
+
+    return 0;
+}
+
+GretlType gretl_type_get_plain_type (GretlType type)
+{
+    int i, n = G_N_ELEMENTS(gretl_type_map);
+
+    for (i=0; i<n; i++) {
+	if (type == gretl_type_map[i].sref) {
+	    return gretl_type_map[i].sing;
+	}
+	if (type == gretl_type_map[i].pref) {
+	    return gretl_type_map[i].plural;
+	}    
+    }
+
+    return 0;
+}
+
+/**
+ * gretl_type_get_name:
+ * @type: a gretl type.
+ *
+ * Returns: the name of @type, or "invalid" on failure.
+ */
+
+const char *gretl_type_get_name (GretlType type)
+{
+    switch (type) {
+    case GRETL_TYPE_BOOL:       return "bool";
+    case GRETL_TYPE_INT:        return "int";
+    case GRETL_TYPE_OBS:        return "obs";
+    case GRETL_TYPE_DOUBLE:     return "scalar";
+    case GRETL_TYPE_SERIES:     return "series";
+    case GRETL_TYPE_USERIES:    return "series";
+    case GRETL_TYPE_MATRIX:     return "matrix";	
+    case GRETL_TYPE_LIST:       return "list";
+    case GRETL_TYPE_BUNDLE:     return "bundle";
+    case GRETL_TYPE_ARRAY:      return "array";
+    case GRETL_TYPE_SCALAR_REF: return "scalar *";
+    case GRETL_TYPE_SERIES_REF: return "series *";
+    case GRETL_TYPE_MATRIX_REF: return "matrix *";
+    case GRETL_TYPE_BUNDLE_REF: return "bundle *";
+    case GRETL_TYPE_STRING:     return "string";
+
+    case GRETL_TYPE_STRINGS:      return "strings";
+    case GRETL_TYPE_MATRICES:     return "matrices";
+    case GRETL_TYPE_BUNDLES:      return "bundles";
+    case GRETL_TYPE_LISTS:        return "lists";
+	
+    case GRETL_TYPE_STRINGS_REF:  return "strings *";
+    case GRETL_TYPE_MATRICES_REF: return "matrices *";
+    case GRETL_TYPE_BUNDLES_REF:  return "bundles *";
+    case GRETL_TYPE_LISTS_REF:    return "lists *";
+
+    case GRETL_TYPE_VOID:       return "void";
+    case GRETL_TYPE_NONE:       return "null";
+    default:
+	return "invalid";
+    }
+}
+
+GretlType gretl_type_from_string (const char *s)
+{
+    if (!strcmp(s, "bool"))     return GRETL_TYPE_BOOL;
+    if (!strcmp(s, "boolean"))  return GRETL_TYPE_BOOL;
+    if (!strcmp(s, "int"))      return GRETL_TYPE_INT;
+    if (!strcmp(s, "obs"))      return GRETL_TYPE_OBS;
+    if (!strcmp(s, "scalar"))   return GRETL_TYPE_DOUBLE;
+    if (!strcmp(s, "series"))   return GRETL_TYPE_SERIES;
+    if (!strcmp(s, "matrix"))   return GRETL_TYPE_MATRIX;
+    if (!strcmp(s, "list"))     return GRETL_TYPE_LIST;
+    if (!strcmp(s, "string"))   return GRETL_TYPE_STRING;
+    if (!strcmp(s, "bundle"))   return GRETL_TYPE_BUNDLE;
+    if (!strcmp(s, "array"))    return GRETL_TYPE_ARRAY;
+
+    if (!strcmp(s, "scalar *"))  return GRETL_TYPE_SCALAR_REF;
+    if (!strcmp(s, "series *"))  return GRETL_TYPE_SERIES_REF;
+    if (!strcmp(s, "matrix *"))  return GRETL_TYPE_MATRIX_REF;
+    if (!strcmp(s, "bundle *"))  return GRETL_TYPE_BUNDLE_REF;
+
+    if (!strcmp(s, "scalarref"))  return GRETL_TYPE_SCALAR_REF;
+    if (!strcmp(s, "seriesref"))  return GRETL_TYPE_SERIES_REF;
+    if (!strcmp(s, "matrixref"))  return GRETL_TYPE_MATRIX_REF;
+    if (!strcmp(s, "bundleref"))  return GRETL_TYPE_BUNDLE_REF;
+
+    if (!strcmp(s, "strings"))   return GRETL_TYPE_STRINGS;
+    if (!strcmp(s, "matrices"))  return GRETL_TYPE_MATRICES;
+    if (!strcmp(s, "bundles"))   return GRETL_TYPE_BUNDLES;
+    if (!strcmp(s, "lists"))     return GRETL_TYPE_LISTS;
+
+    if (!strcmp(s, "strings *"))   return GRETL_TYPE_STRINGS_REF;
+    if (!strcmp(s, "matrices *"))  return GRETL_TYPE_MATRICES_REF;
+    if (!strcmp(s, "bundles *"))   return GRETL_TYPE_BUNDLES_REF;
+    if (!strcmp(s, "lists *"))     return GRETL_TYPE_LISTS_REF;
+
+    if (!strcmp(s, "stringsref"))  return GRETL_TYPE_STRINGS_REF;
+    if (!strcmp(s, "matricesref")) return GRETL_TYPE_MATRICES_REF;
+    if (!strcmp(s, "bundlesref"))  return GRETL_TYPE_BUNDLES_REF;
+    if (!strcmp(s, "listsref"))    return GRETL_TYPE_LISTS_REF;
+
+    return 0;
+}
+
---
+++ lib/src/gretl_typemap.h
@@ -0,0 +1,35 @@
+/* 
+ *  gretl -- Gnu Regression, Econometrics and Time-series Library
+ *  Copyright (C) 2001 Allin Cottrell and Riccardo "Jack" Lucchetti
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#ifndef GRETL_TYPEMAP_H_
+#define GRETL_TYPEMAP_H_
+
+GretlType gretl_type_get_plural (GretlType type);
+
+GretlType gretl_type_get_singular (GretlType type);
+
+GretlType gretl_type_get_ref_type (GretlType type);
+
+GretlType gretl_type_get_plain_type (GretlType type);
+
+const char *gretl_type_get_name (GretlType type);
+
+GretlType gretl_type_from_string (const char *s);
+
+#endif /* GRETL_TYPEMAP_H_ */
