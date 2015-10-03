d9d7a5b16f10be30a72dc6cb03001c28310897f4
Allin Cottrell <cottrell@wfu.edu>
2015-10-03 10:40:39 -0400

    revise build process for addons (getting closer)

---
+++ addons/gig/test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+GRETLCLI="$1"
+
+export GRETL_INCLUDE_PATH=`pwd`
+
+rm -f test.out
+
+for f in examples/*.inp ; do
+  if $GRETLCLI -b $f >> test.out ; then 
+     echo "$f: OK"
+  elif [ `basename $f` = "example7.inp" ] ; then
+     echo "$f: skipped"
+  else
+     echo "$f: failed"
+     break
+  fi
+done
