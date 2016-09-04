2772c2d927884ba9e9f8fb2061b49af0d0766225
Allin Cottrell <cottrell@wfu.edu>
2016-09-04 16:08:04 -0400

    don't print a warning to stderr if it will be shown otherwise?

--- lib/src/gretl_errors.c
+++ lib/src/gretl_errors.c
@@ -367,7 +367,7 @@ void gretl_warnmsg_set (const char *str)
     *gretl_warnmsg = '\0';
     strncat(gretl_warnmsg, str, ERRLEN - 1);
     gretl_warnnum = W_MAX;
-    fputs(str, stderr);
+    /* fputs(str, stderr); */
     fputc('\n', stderr);
 }
 
