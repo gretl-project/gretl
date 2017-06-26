6a9625bae65edad21df6e3c0a481986fe1e30e25
Allin Cottrell <cottrell@wfu.edu>
2017-06-26 11:35:35 -0400

    sync function-package editor UI with revised Model menu

--- gui2/fnsave.c
+++ gui2/fnsave.c
@@ -1018,7 +1018,7 @@ static gint catch_codewin_key (GtkWidget *w, GdkEventKey *event,
 
 	if (word != NULL) {
 	    /* we got a "word": is it the name of a function in
-	       this pachage? */
+	       this package? */
 	    char *active = NULL;
 	    int i;
 
@@ -2456,7 +2456,8 @@ static GtkTreeStore *make_menu_attachment_tree (function_info *finfo,
 	"2 logit\n"
 	"2 probit\n"
 	"1 TSModels\n"
-	"2 CointMenu\n"
+	"2 AR-GLS\n"
+	"2 TSMulti\n"
 	"1 PanelModels\n"
 	"1 RobustModels\n";
     const char *model_items = 
