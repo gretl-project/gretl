2e5a2657c07408b17d19179b96a010711dcae762
Allin Cottrell <cottrell@wfu.edu>
2018-07-17 12:18:21 -0400

    inbundle(); don't give this function special "last_string" treatment

--- lib/src/genparse.h
+++ lib/src/genparse.h
@@ -516,10 +516,9 @@ enum {
 
 /* functions taking a string arg in last position */
 #define string_last_func(s) (s == F_DESEAS || s == F_AGGRBY || \
-			     s == F_INBUNDLE ||	s == F_SSCANF || \
 			     s == F_PRINTF || s == F_SPRINTF || \
 			     s == F_ALLREDUCE || s == F_NORMTEST || \
-			     s == F_NPCORR)
+			     s == F_SSCANF || s == F_NPCORR)
 
 /* functions taking string arg in middle position */
 #define string_mid_func(s) (s == F_REDUCE || s == F_SCATTER)
