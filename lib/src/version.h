22a798e71f34d52ff242cdb38e07148857f4b4b4
Allin Cottrell <cottrell@wfu.edu>
2018-09-03 18:53:51 -0400

    revise change log and version for 2018d-git

--- lib/src/version.h
+++ lib/src/version.h
@@ -20,10 +20,10 @@
 #ifndef GRETL__VERSION_H
 #define GRETL__VERSION_H
 
-#define LIBGRETL_CURRENT   25
+#define LIBGRETL_CURRENT   26
 #define LIBGRETL_REVISION  0
 #define LIBGRETL_AGE       0
 
-#define GRETL_VERSION "2018c-git"
+#define GRETL_VERSION "2018d-git"
 
 #endif /* GRETL__VERSION_H */
