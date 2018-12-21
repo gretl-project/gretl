09b363a6874c0212c260acd78fdceb8a350a79b7
Allin Cottrell <cottrell@wfu.edu>
2018-12-21 15:32:59 -0500

    set version for release

--- lib/src/version.h
+++ lib/src/version.h
@@ -20,10 +20,10 @@
 #ifndef GRETL__VERSION_H
 #define GRETL__VERSION_H
 
-#define LIBGRETL_CURRENT   26
+#define LIBGRETL_CURRENT   27
 #define LIBGRETL_REVISION  0
 #define LIBGRETL_AGE       0
 
-#define GRETL_VERSION "2018d-git"
+#define GRETL_VERSION "2018d"
 
 #endif /* GRETL__VERSION_H */
