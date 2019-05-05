3f96151855d4e0078a39de6467bbaac4c481fa2e
Allin Cottrell <cottrell@wfu.edu>
2019-05-04 20:34:41 -0400

    geneseries(): prefer a string for the second argument

--- lib/src/genparse.h
+++ lib/src/genparse.h
@@ -537,7 +537,7 @@ enum {
 			     s == F_PRINTF || s == F_SPRINTF || \
 			     s == F_ALLREDUCE || s == F_NORMTEST || \
 			     s == F_SSCANF || s == F_NPCORR || \
-			     s == F_INBUNDLE)
+			     s == F_INBUNDLE || s == F_GENSERIES)
 
 /* functions taking string arg in middle position */
 #define string_mid_func(s) (s == F_REDUCE || s == F_SCATTER)
