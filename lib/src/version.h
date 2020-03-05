f65040d177042202cf4bdcb7e023689f24e9610e
Allin Cottrell <cottrell@wfu.edu>
2020-03-05 11:32:32 -0500

    update version info for 2020a release

--- lib/src/version.h
+++ lib/src/version.h
@@ -20,10 +20,10 @@
 #ifndef GRETL__VERSION_H
 #define GRETL__VERSION_H
 
-#define LIBGRETL_CURRENT   31
+#define LIBGRETL_CURRENT   32
 #define LIBGRETL_REVISION  0
 #define LIBGRETL_AGE       0
 
-#define GRETL_VERSION "2020a-git"
+#define GRETL_VERSION "2020a"
 
 #endif /* GRETL__VERSION_H */
