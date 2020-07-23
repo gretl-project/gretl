fc731d021228e035c2181fe2f89d9ff45e7bc181
Allin Cottrell <cottrell@wfu.edu>
2020-07-23 12:31:37 -0400

    graph theme: set default to "dark2" and document the "set" variable

--- gui/settings.c
+++ gui/settings.c
@@ -126,7 +126,7 @@ static char datapage[24] = "Gretl";
 static char scriptpage[24] = "Gretl";
 static char author_mail[32];
 static char sview_style[32] = "classic";
-static char graph_theme[24] = "classic";
+static char graph_theme[24] = "dark2";
 static char gpcolors[18];
 
 static int hc_by_default;
