707f1cccfcc94ecd682b26499bc21eaac90e30db
Allin Cottrell <cottrell@wfu.edu>
2020-07-25 09:21:50 -0400

    for now, get rid of experimental gretl_strsub_on() calls

--- lib/src/monte_carlo.c
+++ lib/src/monte_carlo.c
@@ -1038,10 +1038,7 @@ static int loop_list_refresh (LOOPSET *loop, const DATASET *dset)
     const char *strval = NULL;
     int err = 0;
 
-    if (!gretl_strsub_on()) {
-	/* not doing string substitution */
-	eachvar = get_eachvar_by_name(loop->eachname, &loop->eachtype);
-    } else if (strchr(loop->eachname, '$') != NULL) {
+    if (strchr(loop->eachname, '$') != NULL) {
 	/* $-string substitution required */
 	char vname[VNAMELEN];
 
@@ -1177,7 +1174,7 @@ static int list_loop_setup (LOOPSET *loop, char *s, int *nf)
     while (isspace(*s)) s++;
     tailstrip(s);
 
-    if (gretl_strsub_on() && *s == '@') {
+    if (*s == '@') {
 	/* tricksy: got a list-name that needs string subst? */
 	*loop->eachname = '\0';
 	strncat(loop->eachname, s, VNAMELEN - 1);
@@ -1693,11 +1690,7 @@ static void controller_init (controller *clr)
     clr->vsign = 1;
     clr->expr = NULL;
     clr->genr = NULL;
-    if (gretl_strsub_on()) {
-	clr->subst = -1;
-    } else {
-	clr->subst = 0;
-    }
+    clr->subst = -1;
 }
 
 static void controller_free (controller *clr)
@@ -1721,11 +1714,7 @@ static void loop_cmds_init (LOOPSET *loop, int i1, int i2)
 	loop->cmds[i].ci = 0;
 	loop->cmds[i].opt = 0;
 	loop->cmds[i].genr = NULL;
-	if (!gretl_strsub_on()) {
-	    loop->cmds[i].flags = LOOP_CMD_NOSUB | LOOP_CMD_NODOL;
-	} else {
-	    loop->cmds[i].flags = 0;
-	}
+	loop->cmds[i].flags = 0;
     }
 }
 
