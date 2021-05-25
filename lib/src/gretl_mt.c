67f7f74c40e0b3ddb251f09e5d8ba05d9eb3aac0
Allin Cottrell <cottrell@wfu.edu>
2021-05-25 11:57:10 -0400

    separate out multi-threading settings from libset, in gretl_mt.{c,h}

---
+++ lib/src/gretl_mt.c
@@ -0,0 +1,238 @@
+/*
+ *  gretl -- Gnu Regression, Econometrics and Time-series Library
+ *  Copyright (C) 2001 Allin Cottrell and Riccardo "Jack" Lucchetti
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/* gretl_mt.c for gretl */
+
+#include "libgretl.h"
+#include "libset.h"
+#include "gretl_mt.h"
+
+#ifdef _OPENMP
+# include <omp.h>
+#endif
+
+/* for processors count */
+#if defined(WIN32)
+# include <windows.h>
+#elif defined(OS_OSX)
+# include <sys/param.h>
+# include <sys/sysctl.h>
+#endif
+
+static int omp_n_threads;
+
+#ifdef OS_OSX
+static int omp_mnk_min = -1; /* ? */
+#else
+static int omp_mnk_min = 80000;
+#endif
+
+int gretl_n_processors (void)
+{
+    static int n_proc = -1;
+
+    if (n_proc >= 1) {
+	return n_proc;
+    }
+
+    n_proc = 1;
+
+#if defined(WIN32)
+    SYSTEM_INFO sysinfo;
+
+    GetSystemInfo(&sysinfo);
+    n_proc = sysinfo.dwNumberOfProcessors;
+#elif defined(OS_OSX)
+    int mib[2] = {CTL_HW, HW_NCPU};
+    size_t len = sizeof n_proc;
+
+    if (sysctl(mib, 2, &n_proc, &len, NULL, 0) == -1) {
+	perror("could not determine number of CPUs available");
+	n_proc = 1;
+    }
+#else
+    n_proc = sysconf(_SC_NPROCESSORS_ONLN);
+#endif
+
+    return n_proc;
+}
+
+int gretl_n_physical_cores (void)
+{
+    static int n_cores = -1;
+
+    if (n_cores >= 1) {
+	return n_cores;
+    }
+
+    /* this may well not be what we want, but it's a
+       starting point and fallback
+    */
+    n_cores = gretl_n_processors();
+
+#if defined(WIN32)
+    int nc = win32_get_core_count();
+
+    if (nc > 0) {
+	n_cores = nc;
+    }
+#elif defined(OS_OSX)
+    if (n_cores > 1) {
+	int nc = 0;
+	size_t len = sizeof nc;
+
+	if (sysctlbyname("hw.physicalcpu", &nc, &len, NULL, 0) == -1) {
+	    perror("could not determine number of physical cores available");
+	} else {
+	    n_cores = nc;
+	}
+    }
+#else
+    if (n_cores > 1) {
+	/* check SMT status */
+	FILE *fp = fopen("/sys/devices/system/cpu/smt/active", "r");
+	char line[2];
+	int smt = 0;
+
+	if (fp != NULL) {
+	    if (fgets(line, sizeof line, fp)) {
+		smt = atoi(line);
+	    }
+	    fclose(fp);
+	}
+	if (smt) {
+	    n_cores /= 2;
+	}
+    }
+#endif
+
+    return n_cores;
+}
+
+#ifdef _OPENMP
+
+/* Called only from gretl_matrix.c, where it is guarded by
+   _OPENMP being defined
+*/
+
+int gretl_use_openmp (guint64 n)
+{
+    if (!libset_get_bool(OPENMP_ON) || omp_n_threads < 2) {
+	return 0;
+    } else if (omp_mnk_min >= 0 && n >= (guint64) omp_mnk_min) {
+	return 1;
+    } else {
+	return 0;
+    }
+}
+
+#endif
+
+int set_omp_mnk_min (int n)
+{
+    if (n < -1) {
+	return E_DATA;
+    } else {
+	omp_mnk_min = n;
+	return 0;
+    }
+}
+
+int set_omp_n_threads (int n)
+{
+#if defined(_OPENMP)
+    if (n < 1 || n > gretl_n_processors()) {
+	gretl_errmsg_sprintf(_("omp_num_threads: must be >= 1 and <= %d"),
+			     gretl_n_processors());
+	return E_DATA;
+    } else {
+	omp_n_threads = n;
+	omp_set_num_threads(n);
+	if (blas_is_openblas()) {
+	    blas_set_num_threads(n);
+	}
+    }
+#else
+    gretl_warnmsg_set(_("set_omp_n_threads: OpenMP is not enabled"));
+#endif
+
+    return 0;
+}
+
+void num_threads_init (int blas_type)
+{
+    int nc = gretl_n_physical_cores();
+
+#if defined(_OPENMP)
+    omp_n_threads = nc;
+    omp_set_num_threads(nc);
+#endif
+    if (blas_type == BLAS_OPENBLAS) {
+	blas_set_num_threads(nc);
+    }
+    if (blas_type > BLAS_NETLIB) {
+	set_blas_mnk_min(90000);
+    }
+}
+
+int get_omp_n_threads (void)
+{
+    return omp_n_threads;
+}
+
+#if defined(_OPENMP)
+
+int openmp_by_default (void)
+{
+    static int called = 0;
+    int num_cores = gretl_n_processors();
+    int ret = num_cores > 1;
+
+    if (ret) {
+	/* one can use the environment to turn this off */
+	char *envstr = getenv("GRETL_USE_OPENMP");
+
+	if (envstr != NULL && !strcmp(envstr, "0")) {
+	    ret = 0;
+	}
+    }
+
+    if (!called && ret) {
+	char *s = getenv("OMP_NUM_THREADS");
+
+	if (s != NULL && *s != '\0') {
+	    omp_n_threads = atoi(s);
+	} else {
+	    omp_n_threads = num_cores;
+	}
+	called = 1;
+    }
+
+# if OMP_SHOW
+    if (1) {
+	fprintf(stderr, "number of cores detected = %d\n", num_cores);
+	fprintf(stderr, "use OpenMP by default? %s\n", ret ? "yes" : "no");
+	fprintf(stderr, "omp_num_threads = %d\n", omp_n_threads);
+    }
+# endif
+
+    return ret;
+}
+
+#endif /* _OPENMP defined */
---
+++ lib/src/gretl_mt.h
@@ -0,0 +1,43 @@
+/*
+ *  gretl -- Gnu Regression, Econometrics and Time-series Library
+ *  Copyright (C) 2001 Allin Cottrell and Riccardo "Jack" Lucchetti
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef GRETL_MT_H
+#define GRETL_MT_H
+
+int gretl_n_processors (void);
+
+int gretl_n_physical_cores (void);
+
+void num_threads_init (int blas_type);
+
+int get_omp_n_threads (void);
+
+int set_omp_n_threads (int n);
+
+int set_omp_mnk_min (int n);
+
+#ifdef _OPENMP
+
+int gretl_use_openmp (guint64 n);
+
+int openmp_by_default (void);
+
+#endif
+
+#endif /* GRETL_MT_H */
