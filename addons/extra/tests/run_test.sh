b543dfe6e1cb9493fa22a8c5403decbea02108f4
Artur Tarassow <t.artur@posteo.de>
2021-06-13 13:16:56 +0200

    add combinations() to extra; add test for combinations(); adapt test script of extra package

--- addons/extra/tests/run_test.sh
+++ addons/extra/tests/run_test.sh
@@ -2,20 +2,31 @@
 set -e
 
 DIR=$(dirname $(realpath "$0")) 	# locate folder where this sh-script is located in
-SCRIPT="run_tests_extra.inp"
+SCRIPT_1="test_extra.inp"
+SCRIPT_2="test_extra_matrix.inp"
 PROJECT="extra"
 
 cd $DIR
 echo "Switched to ${DIR}"
 
-gretlcli -b -e -q ${SCRIPT}
-
+gretlcli -b -e -q ${SCRIPT_1}
 if [ $? -eq 0 ]
 then
-  echo "Success: All tests passed for '${PROJECT}'."
-  exit 0
+  echo "Success: All tests passed for '${SCRIPT_1}'."
 else
-  echo "Failure: Tests not passed for '${PROJECT}'." >&2
+  echo "Failure: Tests not passed for '${SCRIPT_1}'." >&2
   exit 1
 fi
 
+gretlcli -b -e -q ${SCRIPT_2}
+if [ $? -eq 0 ]
+then
+  echo "Success: All tests passed for '${SCRIPT_2}'."
+else
+  echo "Failure: Tests not passed for '${SCRIPT_2}'." >&2
+  exit 1
+fi
+
+
+echo "Success: All tests passed."
+exit 0
