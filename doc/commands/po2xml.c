f96f7f572558f4df93b07b7bb6a3c950b4b39853
Allin Cottrell <cottrell@wfu.edu>
2021-08-01 13:23:29 -0400

    make comments more explanatory

--- doc/commands/po2xml.c
+++ doc/commands/po2xml.c
@@ -1,9 +1,12 @@
-/* This can be used (possibly adapted) to retrieve translations from
+/* This program can be used to retrieve translations from
    gretl *.po files and write them out in the form wanted for
-   XSLT. The example below works on the names of "funclist" elements
+   XSLT.
+
+   The example below works on the names of "funclist" elements
    in translations of gretl_functions_en.xml, with the aim of
    making them come out correctly in the TeX/PDF version of the
-   Gretl Reference.
+   Gretl Reference. The LANG setting when the program is run
+   determines the language of the translated strings.
 
    Allin Cottrell, August 2021
 */
@@ -21,6 +24,7 @@ void nls_init (void)
 
 int main (void)
 {
+    /* translatable strings from gui/textbuf.c */
     const char *names_en[] = {
 	"Accessors",
 	"Built-in strings",
@@ -31,6 +35,7 @@ int main (void)
 
     nls_init();
 
+    /* output lines that can be inserted into hlpstrs_<lang>.xml */
     for (i=0; names_en[i] != NULL; i++) {
 	printf(" <gentext key=\"%s\" text=\"%s\" />\n", names_en[i],
 	       _(names_en[i]));
