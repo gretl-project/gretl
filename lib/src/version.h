9b2b8f110001551010e3e9c79bc7c7ea37c1a24f
Allin Cottrell <cottrell@wfu.edu>
2022-11-01 12:24:57 -0400

    update version info for release

--- lib/src/version.h
+++ lib/src/version.h
@@ -20,10 +20,10 @@
 #ifndef GRETL__VERSION_H
 #define GRETL__VERSION_H
 
-#define LIBGRETL_CURRENT   42
+#define LIBGRETL_CURRENT   43
 #define LIBGRETL_REVISION  0
 #define LIBGRETL_AGE       0
 
-#define GRETL_VERSION "2022c-git"
+#define GRETL_VERSION "2022c"
 
 #endif /* GRETL__VERSION_H */
