004eef19f93eb868ba421ed7bb5143b2ad87b1cf
Allin Cottrell <cottrell@wfu.edu>
2023-05-13 08:08:47 -0400

    remove the "rawregls" alias for _regls()

--- lib/src/genlex.c
+++ lib/src/genlex.c
@@ -617,7 +617,6 @@ struct str_table func_alias[] = {
     { F_CNAMEGET, "colname" },
     { F_RNAMEGET, "rowname" },
     { F_EXISTS,   "isnull" }, /* deprecated */
-    { HF_REGLS,   "rawregls" },
     { 0,          NULL }
 };
 
