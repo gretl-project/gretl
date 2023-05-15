f848bff08f6768dafc8ac60a9849effdc27f6600
Allin Cottrell <cottrell@wfu.edu>
2023-05-15 10:32:27 -0400

    streamline the gretlzip code

--- utils/dbzip/gretlzip.c
+++ utils/dbzip/gretlzip.c
@@ -5,7 +5,8 @@
 
    Further revised February 2003 to allow for inclusion of a database
    codebook. Than again in January 2018 to allow for the codebook to
-   be a PDF file.
+   be a PDF file. May 2023: streamline the code and make it a bit more
+   robust.
 */
 
 #include <stdio.h>
@@ -40,24 +41,22 @@ static void print_time_short (char *buf, const time_t *timep)
     char timebuf[32];
 
     ztime = localtime(timep);
-    sprintf(timebuf, "%4d%02d%02d", 
-	    ztime->tm_year + 1900, 
+    sprintf(timebuf, "%4d%02d%02d",
+	    ztime->tm_year + 1900,
 	    ztime->tm_mon + 1,
 	    ztime->tm_mday);
     strcat(buf, timebuf);
-}
-
-static void clear (char *buf, size_t len)
-{
-    memset(buf, 0, len);
 }
 
 static char *strip_path (char *fname)
 {
     char *p = strrchr(fname, PATHSEP);
 
-    if (p != NULL && *(p + 1)) return p + 1;
-    else return fname;
+    if (p != NULL && *(p + 1)) {
+        return p + 1;
+    } else {
+        return fname;
+    }
 }
 
 static char *switch_ext (char *fname, char *ext)
@@ -72,47 +71,54 @@ static char *switch_ext (char *fname, char *ext)
     return fname;
 }
 
-static int parse_db_header (const char *buf, size_t *idxlen, 
+static int parse_db_header (const char *buf, size_t *idxlen,
 			    size_t *datalen, size_t *cblen,
 			    int *pdfdoc)
 {
-    char *p;
-    int err = 0;
+    const char *p;
 
     *cblen = 0;
 
     /* length of index file (required) */
     if (sscanf(buf, "%lu", idxlen) != 1) {
-	err = 1;
+	return 1;
     }
 
-    if (!err) {
-	/* length of data (required) */
-	p = strchr(buf, '\n');
-	if (p == NULL) {
-	    err = 1;
-	} else {
-	    p++; 
-	    if (sscanf(p, "%lu", datalen) != 1) {
-		err = 1;
-	    }
-	}
+    /* length of data (required) */
+    p = strchr(buf, '\n');
+    if (p == NULL) {
+        return 1;
+    } else {
+        p++;
+        if (sscanf(p, "%lu", datalen) != 1) {
+            return 1;
+        }
     }
 
-    if (!err) {
-	/* codebook info (optional) */
-	p = strchr(p, '\n');
-	if (p != NULL) {
-	    p++;
-	    if (sscanf(p, "%lu", cblen) != 1) {
-		*cblen = 0;
-	    } else if (strstr(p, ".pdf")) {
-		*pdfdoc = 1;
-	    }
-	}
+    /* codebook info (optional) */
+    p = strchr(p, '\n');
+    if (p != NULL) {
+        p++;
+        if (sscanf(p, "%lu", cblen) != 1) {
+            *cblen = 0;
+        } else if (strstr(p, ".pdf")) {
+            *pdfdoc = 1;
+        }
     }
 
-    return err;
+
+    return 0;
+}
+
+static void close_infiles (FILE *infiles[], int n)
+{
+    int i;
+
+    for (i=0; i<n; i++) {
+        if (infiles[i] != NULL) {
+            fclose(infiles[i]);
+        }
+    }
 }
 
 /* Note: below -- the "info" string for the archive must be
@@ -121,213 +127,166 @@ static int parse_db_header (const char *buf, size_t *idxlen,
 
 static int ggz_create (char *infobuf, char *fname, char *gzname)
 {
-    int gotcb = 0;
+    int n_files = 2;
     int i, len, chk;
     struct stat fbuf;
-    FILE *fidx, *fbin, *fcb;
+    char fnames[3][MAXLEN] = {0};
+    FILE *infiles[3] = {NULL};
     char tmp[40];
-    char idxname[MAXLEN], binname[MAXLEN], cbname[MAXLEN];
-    char *readname = NULL;
     char gzbuf[BUFSIZE];
     gzFile fgz;
 
-    sprintf(idxname, "%s.idx", fname);
-    sprintf(binname, "%s.bin", fname);
-    sprintf(cbname, "%s.cb", fname);
+    sprintf(fnames[0], "%s.idx", fname);
+    sprintf(fnames[1], "%s.bin", fname);
+    sprintf(fnames[2], "%s.cb",  fname);
     strcat(gzname, ".gz");
 
-    fidx = fopen(idxname, "rb");
-    if (fidx == NULL) {
-	sprintf(infobuf, "Couldn't open %s for reading\n", idxname);
-	return 1;
+    for (i=0; i<2; i++) {
+        infiles[i] = fopen(fnames[i], "rb");
+        if (infiles[i] == NULL) {
+            sprintf(infobuf, "Couldn't open %s for reading\n", fnames[i]);
+            return 1;
+        }
     }
 
-    fbin = fopen(binname, "rb");
-    if (fbin == NULL) {
-	sprintf(infobuf, "Couldn't open %s for reading\n", binname);
-	fclose(fidx);
-	return 1;
-    }
-
-    fcb = fopen(cbname, "rb");
-    if (fcb != NULL) {
+    infiles[2] = fopen(fnames[2], "rb");
+    if (infiles[2] != NULL) {
 	/* plain text codebook */
-	printf("Found codebook file %s\n", cbname);
-	gotcb = 1;
+	printf("Found codebook file %s\n", fnames[2]);
+	n_files = 3;
     } else {
 	/* try for PDF? */
-	sprintf(cbname, "%s.pdf", fname);
-	fcb = fopen(cbname, "rb");
-	if (fcb != NULL) {
-	    printf("Found codebook file %s\n", cbname);
-	    gotcb = 1;
+	sprintf(fnames[2], "%s.pdf", fname);
+	infiles[2] = fopen(fnames[2], "rb");
+	if (infiles[2] != NULL) {
+	    printf("Found codebook file %s\n", fnames[2]);
+	    n_files = 3;
 	}
     }
 
     fgz = gzopen(gzname, "wb");
     if (fgz == NULL) {
 	sprintf(infobuf, "Couldn't open %s for writing\n", gzname);
-	fclose(fidx);
-	fclose(fbin);
-	if (fcb != NULL) {
-	    fclose(fcb);
-	}
+        close_infiles(infiles, n_files);
 	return 1;
     }
 
-    for (i=0; i<((gotcb)? 3 : 2); i++) {
-	if (i == 0) readname = idxname;
-	else if (i == 1) readname = binname;
-	else if (i == 2) readname = cbname;
-
-	if (stat(readname, &fbuf)) {
-	    sprintf(infobuf, "Error stat'ing %s\n", readname);
+    for (i=0; i<n_files; i++) {
+	if (stat(fnames[i], &fbuf)) {
+	    sprintf(infobuf, "Error stat'ing %s\n", fnames[i]);
 	    return 1;
-	} 
-
+	}
 	sprintf(tmp, "%8lu ", fbuf.st_size);
 	strcat(infobuf, tmp);
-	if (gotcb) {
+	if (n_files == 3) {
 	    print_time_short(infobuf, &(fbuf.st_mtime));
 	} else {
 	    print_time_long(infobuf, &(fbuf.st_mtime));
 	}
-	sprintf(tmp, "%15s", strip_path(readname));
+	sprintf(tmp, "%.15s", strip_path(fnames[i]));
 	strcat(infobuf, tmp);
 	strcat(infobuf, "\n");
     }
 
     printf("infobuf: strlen = %d\n", (int) strlen(infobuf));
-    gzwrite(fgz, infobuf, INFOLEN); 
+    gzwrite(fgz, infobuf, INFOLEN);
 
-    /* write compressed content of idx and bin files */
-    while ((len = fread(gzbuf, 1, BUFSIZE, fidx)) > 0) {
-	chk = gzwrite(fgz, gzbuf, len);
-	if (chk != len) 
-	    fprintf(stderr, "*** gzwrite: len = %d but chk = %d\n", len, chk);
+    /* write compressed content of input files */
+    for (i=0; i<n_files; i++) {
+        while ((len = fread(gzbuf, 1, BUFSIZE, infiles[i])) > 0) {
+            chk = gzwrite(fgz, gzbuf, len);
+            if (chk != len) {
+                fprintf(stderr, "*** gzwrite: len = %d but chk = %d\n", len, chk);
+            }
+        }
     }
 
-    while ((len = fread(gzbuf, 1, BUFSIZE, fbin)) > 0) {
-	chk = gzwrite(fgz, gzbuf, len);
-	if (chk != len) 
-	    fprintf(stderr, "*** gzwrite: len = %d but chk = %d\n", len, chk);
-    }
-
-    if (gotcb) {
-	while ((len = fread(gzbuf, 1, BUFSIZE, fcb)) > 0) {
-	    chk = gzwrite(fgz, gzbuf, len);
-	    if (chk != len) 
-	        fprintf(stderr, "*** gzwrite: len = %d but chk = %d\n", len, chk);
-	}
-    }
-
-    fclose(fidx);
-    fclose(fbin);
-    if (gotcb) {
-	fclose(fcb); 
-    }
-
+    close_infiles(infiles, n_files);
     gzclose(fgz);
-    
+
     return 0;
 }
 
 static int ggz_extract (char *infobuf, char *fname, char *outname)
 {
-    int fidx, fbin, fcb = -1;
-    size_t idxlen, datalen, cblen, bytesleft;
-    int bgot, pdfdoc = 0;
-    char idxname[MAXLEN], binname[MAXLEN], cbname[MAXLEN];
-    char gzbuf[BUFSIZE];
+    int fids[3] = {-1, -1, -1};
+    size_t sizes[3] = {0};
+    size_t u, umax, rem;
+    int bgot, wrote, pdfdoc = 0;
+    char outnames[3][MAXLEN] = {0};
+    char gzbuf[BUFSIZE] = {0};
     gzFile fgz;
-    unsigned i;
-    int err = 0;
+    int n_files = 2;
+    int i, err = 0;
 
+    /* initial check on gzipped input file */
     strcat(fname, ".gz");
-    sprintf(idxname, "%s.idx", outname);
-    sprintf(binname, "%s.bin", outname);
-    cbname[0] = '\0';
-
     fgz = gzopen(fname, "rb");
     if (fgz == NULL) {
 	sprintf(infobuf, "Couldn't gzopen %s for reading\n", fname);
 	return 1;
     }
 
-    fidx = creat(idxname, 00644);
-    if (fidx == -1) {
-	gzclose(fgz);
-	sprintf(infobuf, "Couldn't open %s for writing\n", idxname);
-	return 1;
-    }
-
-    fbin = creat(binname, 00644);
-    if (fbin == -1) {
-	gzclose(fgz);
-	close(fidx);
-	sprintf(infobuf, "Couldn't open '%s' for writing\n"
-		"Error: %s\n", binname, strerror(errno));
-	return 1;
-    }
-
-    clear(gzbuf, BUFSIZE);
     gzread(fgz, gzbuf, INFOLEN);
     strcpy(infobuf, gzbuf);
 
-    if (parse_db_header(infobuf, &idxlen, &datalen, &cblen, &pdfdoc)) {
+    if (parse_db_header(infobuf, &sizes[0], &sizes[1], &sizes[2], &pdfdoc)) {
 	fputs("Error reading info buffer: failed to get byte counts\n",
 	      stderr);
-	err = 1;
-	goto bailout;
-    } else if (cblen > 0) {
+        gzclose(fgz);
+        return 1;
+    }
+
+    /* set up output filenames */
+    sprintf(outnames[0], "%s.idx", outname);
+    sprintf(outnames[1], "%s.bin", outname);
+    if (sizes[2] > 0) {
+        /* got a codebook buffer */
 	if (pdfdoc) {
 	    fputs("Detected PDF codebook\n", stderr);
-	    sprintf(cbname, "%s.pdf", outname);
+	    sprintf(outnames[2], "%s.pdf", outname);
 	} else {
 	    fputs("Detected plain text codebook\n", stderr);
-	    sprintf(cbname, "%s.cb", outname);
+	    sprintf(outnames[2], "%s.cb", outname);
 	}
-	fcb = creat(cbname, 00644);
-	if (fcb == -1) {
-	    sprintf(infobuf, "Couldn't open '%s' for writing\n"
-		    "Error: %s\n", cbname, strerror(errno));
-	    err = 1;
-	    goto bailout;
-	}	
+        n_files = 3;
     }
 
-    for (i=0; i<1+idxlen/BUFSIZE; i++) {
-	bytesleft = idxlen - BUFSIZE * i;
-	if (bytesleft <= 0) break;
-	bgot = gzread(fgz, gzbuf, (bytesleft > BUFSIZE)? BUFSIZE : bytesleft);
-	write(fidx, gzbuf, bgot);
+    /* open output files for writing */
+    for (i=0; i<n_files; i++) {
+        fids[i] = creat(outnames[i], 00644);
+        if (fids[i] == -1) {
+            gzclose(fgz);
+            sprintf(infobuf, "Couldn't open %s for writing\n"
+                    "Error: %s\n", outnames[i], strerror(errno));
+            err = 1;
+            break;
+        }
     }
 
-    for (i=0; i<1+datalen/BUFSIZE; i++) {
-	bytesleft = datalen - BUFSIZE * i;
-	if (bytesleft <= 0) break;
-	bgot = gzread(fgz, gzbuf, (bytesleft > BUFSIZE)? BUFSIZE : bytesleft);
-	write(fbin, gzbuf, bgot);
+    /* write the decompressed data */
+    for (i=0; i<n_files && !err; i++) {
+        umax = 1 + sizes[i] / BUFSIZE;
+        for (u=0; u<umax; u++) {
+            rem = sizes[i] - BUFSIZE * u;
+            if (rem <= 0) break;
+            bgot = gzread(fgz, gzbuf, (rem > BUFSIZE)? BUFSIZE : rem);
+            wrote = write(fids[i], gzbuf, bgot);
+            if (wrote != bgot) {
+                sprintf(infobuf, "%s: bytes written %d, should be %d\n",
+                        outnames[i], wrote, bgot);
+                err = 1;
+            }
+        }
     }
-
-    if (cblen > 0) {
-	for (i=0; i<1+cblen/BUFSIZE; i++) {
-	    bytesleft = cblen - BUFSIZE * i;
-	    if (bytesleft <= 0) break;
-	    bgot = gzread(fgz, gzbuf, (bytesleft > BUFSIZE)? BUFSIZE : bytesleft);
-	    write(fcb, gzbuf, bgot);
-	}
-    }
-
- bailout:
 
     gzclose(fgz);
-    close(fidx);
-    close(fbin);
-    if (fcb != -1) {
-	close(fcb);
+    for (i=0; i<n_files; i++) {
+        if (fids[i] != -1) {
+            close(fids[i]);
+        }
     }
-    
+
     return err;
 }
 
@@ -339,17 +298,19 @@ static void usage (char *progname)
 	    " %s -x foo extracts foo.idx and foo.bin from foo.gz\n\n",
 	    progname, progname);
     fputs("Option: if a second basename is supplied, it is used "
-	  "for the output file(s).\n", stderr); 
-    fputs("If a codebook file (.cb) is found on archive creation, it is rolled\n"
+	  "for the output file(s).\n", stderr);
+    fputs("If a codebook file (.cb or .pdf) is found on archive creation, it is rolled\n"
 	  "into the archive.\n",
-	  stderr); 
+	  stderr);
     exit(EXIT_FAILURE);
 }
 
 int main (int argc, char *argv[])
 {
     int err, create = 0;
-    char fname[MAXLEN], outname[MAXLEN], infobuf[INFOLEN];
+    char infobuf[INFOLEN] = {0};
+    char fname[MAXLEN];
+    char outname[MAXLEN];
     char *callname;
     int unzip = 0, filearg = 2;
 
@@ -365,32 +326,31 @@ int main (int argc, char *argv[])
         unzip = 1;
     }
 
-    if ((unzip && argc != 2) || (!unzip && argc < 3)) 
+    if ((unzip && argc != 2) || (!unzip && argc < 3)) {
 	usage(argv[0]);
+    }
 
     if (!strcmp(argv[1], "-c")) {
 	create = 1;
     } else if (!unzip && strcmp(argv[1], "-x")) {
 	usage(argv[0]);
     }
-	
-    strncpy(fname, argv[filearg], MAXLEN-1);
-    fname[MAXLEN-1] = 0;
 
-    *outname = 0;
+    *fname = '\0';
+    strncat(fname, argv[filearg], MAXLEN-1);
+
+    *outname = '\0';
     if (argc == 4) {
 	strncat(outname, argv[filearg + 1], MAXLEN-1);
     } else {
 	strcpy(outname, fname);
     }
-	
-    switch_ext(fname, ""); 
+
+    switch_ext(fname, "");
     switch_ext(outname, "");
     fprintf(stderr, "Taking input from %s%s\nWriting output to %s%s\n",
-	    fname, (create)? " (.idx, .bin)": ".gz", 
+	    fname, (create)? " (.idx, .bin)": ".gz",
 	    outname, (create)? ".gz" : " (.idx, .bin)");
-
-    clear(infobuf, INFOLEN);
 
     if (create) {
 	err = ggz_create(infobuf, fname, outname);
@@ -408,6 +368,3 @@ int main (int argc, char *argv[])
 
     return 0;
 }
-
-
-
