76f531eaff50af527fb94842d24ce63b41de87e4
Allin Cottrell <cottrell@wfu.edu>
2023-12-28 11:25:53 -0500

    put the gretl version into addons.xml

--- addons/gfnindex.c
+++ addons/gfnindex.c
@@ -33,15 +33,45 @@ int process_index_entry (const char *pkg)
     return 0;
 }
 
+int get_gretl_version (const char *path, char *targ)
+{
+    char *p, line[256];
+    char vh[512];
+    FILE *fp;
+
+    sprintf(vh, "%s/lib/src/version.h", path);
+    fp = fopen(vh, "r");
+    if (fp == NULL) {
+	return 1;
+    }
+
+    while (fgets(line, sizeof line, fp)) {
+	if (!strncmp(line, "#define GRETL_VERSION ", 22)) {
+	    p = line + 23;
+	    strncat(targ, p, strcspn(p, "\""));
+	    break;
+	}
+    }
+    fclose(fp);
+
+    return 0;
+}
+
 int main (int argc, char **argv)
 {
+    char vstr[10] = {0};
     int i, err = 0;
+
+    err = get_gretl_version(argv[1], vstr);
+    if (err) {
+	exit(EXIT_FAILURE);
+    }
 
     fputs("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n", stdout);
     fputs("<?xml-stylesheet type=\"text/xsl\" href=\"addons.xsl\"?>\n", stdout);
-    fputs("<gretl-addons>\n", stdout);
+    printf("<gretl-addons version=\"%s\">\n", vstr);
 
-    for (i=1; i<argc && !err; i++) {
+    for (i=2; i<argc && !err; i++) {
 	err = process_index_entry(argv[i]);
     }
 
