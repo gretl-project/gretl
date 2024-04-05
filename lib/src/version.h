08da113621070a15c529b231f0302093088dd38c
Allin Cottrell <cottrell@wfu.edu>
2024-04-05 12:26:30 -0400

    bump gretl version for release

--- lib/src/version.h
+++ lib/src/version.h
@@ -20,10 +20,10 @@
 #ifndef GRETL__VERSION_H
 #define GRETL__VERSION_H
 
-#define LIBGRETL_CURRENT   46
+#define LIBGRETL_CURRENT   47
 #define LIBGRETL_REVISION  0
 #define LIBGRETL_AGE       0
 
-#define GRETL_VERSION "2024a-git"
+#define GRETL_VERSION "2024a"
 
 #endif /* GRETL__VERSION_H */
