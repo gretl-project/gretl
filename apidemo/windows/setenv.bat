41d5145a39e8d6876b62be0b7ced49f1c3299249
Allin Cottrell <cottrell@wfu.edu>
2024-10-31 09:58:54 -0400

    move msc files to Files area

--- apidemo/windows/setenv.bat
+++
@@ -1,3 +0,0 @@
-set "RES=C:\Program Files\gretl"
-set "GRETL_PLUGIN_PATH=%RES%\plugins"
-set "GRETL_DATA_PATH=%RES%\data"
--- apidemo/windows/setenv.ps1
+++
@@ -1,3 +0,0 @@
-$Env:RES = "C:\Program Files\gretl"
-$Env:GRETL_PLUGIN_PATH = "$Env:RES\plugins"
-$Env:GRETL_DATA_PATH = "$Env:RES\data"
