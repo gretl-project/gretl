27f47c06de81b84ec3589ea3657c5f7c6b8c5746
Allin Cottrell <cottrell@wfu.edu>
2025-03-15 12:41:40 -0400

    fix conditionality at one point

--- lib/src/kalman.c
+++ lib/src/kalman.c
@@ -4993,10 +4993,12 @@ static int kfilter_univariate (kalman *K, PRN *prn)
         Kki = ui->Kki;
     }
 
-    if (K->n > 1 && matrix_is_varying(K, K_ZT)) {
-        err = kalman_ensure_stepinfo(K, 0);
-        if (err) {
-            return err;
+    if (K->n > 1) {
+        if (matrix_is_varying(K, K_ZT)) {
+            err = kalman_ensure_stepinfo(K, 0);
+            if (err) {
+                return err;
+            }
         }
         UF = ui->UF;
     }
