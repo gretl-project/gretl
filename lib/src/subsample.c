386cfe5eee6ee118b2785014032165e3d01d037f
Allin Cottrell <cottrell@wfu.edu>
2025-03-29 16:27:01 -0400

    tweak for a function aimed at third-party usage

--- lib/src/subsample.c
+++ lib/src/subsample.c
@@ -3643,35 +3643,37 @@ void print_sample_status (const DATASET *dset, PRN *prn)
 /**
  * data_report:
  * @dset: data information struct.
- * @fname: filename for current datafile.
+ * @fname: filename for current datafile, or NULL.
  * @prn: gretl printing struct.
  *
  * Write out a summary of the content of the current data set.
  *
  * Returns: 0 on successful completion, non-zero on error.
- *
  */
 
 int data_report (const DATASET *dset, const char *fname, PRN *prn)
 {
-    char startdate[OBSLEN], enddate[OBSLEN], tmp[MAXLEN];
-    char tstr[48];
+    char startobs[OBSLEN], endobs[OBSLEN], tmp[MAXLEN];
     const char *vlabel;
     int i;
 
-    ntolabel(startdate, 0, dset);
-    ntolabel(enddate, dset->n - 1, dset);
+    ntolabel(startobs, 0, dset);
+    ntolabel(endobs, dset->n - 1, dset);
 
-    sprintf(tmp, _("Data file %s\nas of"),
-	    (*fname != '\0')? fname : _("(unsaved)"));
+    if (fname != NULL) {
+        /* the original code for this function */
+        char tstr[48];
 
-    print_time(tstr);
-    pprintf(prn, "%s %s\n\n", tmp, tstr);
+        sprintf(tmp, _("Data file %s\nas of"),
+                (*fname != '\0')? fname : _("(unsaved)"));
+        print_time(tstr);
+        pprintf(prn, "%s %s\n\n", tmp, tstr);
 
-    if (dset->descrip != NULL && *dset->descrip != '\0') {
-	pprintf(prn, "%s:\n\n", _("Description"));
-	pputs(prn, dset->descrip);
-	pputs(prn, "\n\n");
+        if (dset->descrip != NULL && *dset->descrip != '\0') {
+            pprintf(prn, "%s:\n\n", _("Description"));
+            pputs(prn, dset->descrip);
+            pputs(prn, "\n\n");
+        }
     }
 
     dataset_type_string(tmp, dset);
@@ -3682,8 +3684,13 @@ int data_report (const DATASET *dset, const char *fname, PRN *prn)
 	pprintf(prn, "%s: %s\n", _("Frequency"), tmp);
     }
 
-    pprintf(prn, "%s: %s - %s (n = %d)\n\n", _("Range"),
-	    startdate, enddate, dset->n);
+    if (dataset_is_panel(dset)) {
+        pprintf(prn, "%s: N = %d, T = %d\n", _("Panel data"),
+                dset->n / dset->pd, dset->pd);
+    } else {
+        pprintf(prn, "%s: %s - %s (n = %d)\n\n", _("Range"),
+                startobs, endobs, dset->n);
+    }
 
     pprintf(prn, "%s:\n\n", _("Listing of variables"));
 
