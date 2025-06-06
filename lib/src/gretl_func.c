a7d02469f5b82bee75481d545d86982b88ef3c05
Allin Cottrell <cottrell@wfu.edu>
2025-06-06 11:09:17 -0400

    cut down some whitespace

--- lib/src/gretl_func.c
+++ lib/src/gretl_func.c
@@ -3446,7 +3446,6 @@ static int package_write_index (fnpkg *pkg, PRN *inprn)
     }
 
     gretl_xml_header(prn);
-
     pprintf(prn, "<gretl-addon name=\"%s\"", pkg->name);
 
     if (pkg->dreq == FN_NEEDS_TS) {
@@ -3458,23 +3457,19 @@ static int package_write_index (fnpkg *pkg, PRN *inprn)
     } else if (pkg->dreq == FN_NODATA_OK) {
         pprintf(prn, " %s=\"true\"", NO_DATA_OK);
     }
-
     if (pkg->modelreq > 0) {
         pprintf(prn, " model-requirement=\"%s\"",
                 gretl_command_word(pkg->modelreq));
     }
-
     if (pkg->minver > 0) {
         char vstr[10];
 
         pprintf(prn, " minver=\"%s\"",
                 gretl_version_string(vstr, pkg->minver));
     }
-
     if (pkg->uses_subdir) {
         pputs(prn, " lives-in-subdir=\"true\"");
     }
-
     if (pkg->data_access) {
         pputs(prn, " wants-data-access=\"true\"");
     }
@@ -3556,7 +3551,6 @@ static int real_write_function_package (fnpkg *pkg, PRN *prn, int mpi)
         pprintf(prn, " model-requirement=\"%s\"",
                 gretl_command_word(pkg->modelreq));
     }
-
     if (pkg->minver > 0) {
         char vstr[10];
 
@@ -3588,7 +3582,6 @@ static int real_write_function_package (fnpkg *pkg, PRN *prn, int mpi)
     if (pkg->label != NULL) {
         gretl_xml_put_tagged_string("label", pkg->label, prn);
     }
-
     if (pkg->mpath != NULL) {
         gretl_xml_put_tagged_string("menu-attachment", pkg->mpath, prn);
     }
@@ -3601,7 +3594,6 @@ static int real_write_function_package (fnpkg *pkg, PRN *prn, int mpi)
         gretl_xml_put_string(trim_text(pkg->help), prn);
         pputs(prn, "\n</help>\n");
     }
-
     if (pkg->gui_help != NULL && !mpi) {
         if (pkg->gui_help_fname != NULL) {
             pprintf(prn, "<gui-help filename=\"%s\">\n",
@@ -3618,7 +3610,6 @@ static int real_write_function_package (fnpkg *pkg, PRN *prn, int mpi)
                                     (const char **) pkg->datafiles,
                                     pkg->n_files, prn);
     }
-
     if (pkg->depends != NULL) {
         gretl_xml_put_strings_array("depends",
                                     (const char **) pkg->depends,
