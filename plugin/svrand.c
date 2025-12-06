49bbf0430e15bb5ecd9c3e16de8656b4103b069d
Allin Cottrell <cottrell@wfu.edu>
2025-12-06 14:34:31 -0500

    add dedicated RNG for SVM for backward compatibility

---
+++ plugin/svrand.c
@@ -0,0 +1,56 @@
+/* dedicated RNG for use in generating random folds dir use with SVM */
+
+#include <stdint.h>
+
+static uint64_t smx;
+
+static inline uint64_t splitmix64_next() {
+    uint64_t z = (smx += 0x9e3779b97f4a7c15);
+        
+    z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
+    z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
+    return z ^ (z >> 31);
+}
+
+static inline uint64_t xor_rotl (const uint64_t x, int k) {
+    return (x << k) | (x >> (64 - k));
+}
+
+static uint64_t s[4];
+
+static void set_xor_state (uint64_t u)
+{
+    s[0] = smx = u;
+    s[1] = splitmix64_next();
+    s[2] = splitmix64_next();
+    s[3] = splitmix64_next();
+}
+
+static uint64_t xor_i64 (void) {
+    const uint64_t ret = s[0] + s[3];
+    const uint64_t t = s[1] << 17;
+
+    s[2] ^= s[0];
+    s[3] ^= s[1];
+    s[1] ^= s[2];
+    s[0] ^= s[3];
+
+    s[2] ^= t;
+    s[3] = xor_rotl(s[3], 45);
+
+    return ret;
+}
+
+void svrand_init (uint64_t seed)
+{
+    if (seed > 0) {
+        set_xor_state(seed);
+    } else {
+        set_xor_seed((uint64_t) time(NULL));
+    }
+}
+
+uint32_t svrand (void)
+{
+    return (uint32_t) (xor_i64() >> 32);
+}
---
+++ plugin/svrand.h
@@ -0,0 +1,10 @@
+/* header for svrand.c, for SVM use */
+
+#ifndef SVRAND_H
+#define SVRAND_H
+
+void svrand_init (uint64_t seed);
+
+uint32_t svrand (void);
+
+#endif /* SVRAND_H */ 
